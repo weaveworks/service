36bbee1d912f0e5417d44a11d8e33e8c188d43b0
Tom Wilkie <tom@weave.works>
2015-08-04 16:56:27 +0000

    Move to ubuntu for nginx as it seems to work...

--- run.sh
+++ run.sh
@@ -11,17 +11,18 @@ REPLICAS=${REPLICAS:-1}
 
 start_container() {
     IMAGE=$1
-    NAME=${2:-$1}
+    NAME=$2
+    shift 2
 
     for i in $(seq $REPLICAS); do
         if docker inspect $NAME$i >/dev/null 2>&1; then
             docker rm -f $NAME$i
         fi
-        docker run -d --name=$NAME$i $IMAGE
+        docker run $@ -d --name=$NAME$i --hostname=$NAME.weave.local $IMAGE
     done
 }
 
 start_container weaveworks/users users
 start_container weaveworks/app-mapper app-mapper
 start_container weaveworks/ui-server ui-server
-start_container weaveworks/frontend frontend
+start_container weaveworks/frontend frontend --add-host=dns.weave.local:$(weave docker-bridge-ip)
