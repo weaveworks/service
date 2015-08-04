57b2736b1951cd5ed4945df9b6c6ddeb3932f3e5
Tom Wilkie <tom@weave.works>
2015-08-04 10:59:26 +0000

    Add basic nginx frontend server
    Currently it does a dns lookup on every request; these should be cached with lua magic at some point.

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -eux
+
+for dir in app-mapper client users frontend; do
+    make -C $dir clean
+    make -C $dir image.tar
+done
+
+REPLICAS=${REPLICAS:-1}
+
+start_container() {
+    IMAGE=$1
+    NAME=${2:-$1}
+
+    for i in $(seq $REPLICAS); do
+        if docker inspect $NAME$i >/dev/null 2>&1; then
+            docker rm -f $NAME$i
+        fi
+        docker run -d --name=$NAME$i $IMAGE
+    done
+}
+
+start_container weaveworks/users users
+start_container weaveworks/app-mapper app-mapper
+start_container weaveworks/ui-server ui-server
+start_container weaveworks/frontend frontend
