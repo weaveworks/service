de1f6c2f033078086ea861f2b606c094d90df55c
David Kaltschmidt <david.kaltschmidt@gmail.com>
2015-08-12 18:04:49 +0200

    hide user invite form for now
    fixes #78

--- client/src/pages/organization/users.jsx
+++ client/src/pages/organization/users.jsx
@@ -60,44 +60,45 @@ export default class Users extends React.Component {
             {users}
           </List>
         </Box>
-        <div style={formStyle}>
-          <TextField hintText="Email" ref="emailField" />
-          <RaisedButton label="Invite" style={buttonStyle} onClick={this._handleInviteTouchTap.bind(this)} />
-        </div>
+        // <div style={formStyle}>
+        //   <TextField hintText="Email" ref="emailField" />
+        //   <RaisedButton label="Invite" style={buttonStyle} onClick={this._handleInviteTouchTap.bind(this)} />
+        // </div>
       </div>
     );
   }
 
-  _handleDeleteTouchTap(user) {
-    let url = `/api/org/${this.props.org}/users/${user.id}`;
 
-    deleteData(url)
-      .then(function(resp) {
-        this.setState({
-          users: resp
-        });
-        this.refs.emailField.setValue("");
-      }.bind(this), function(resp) {
-        // TODO show error
-      }.bind(this));
-  }
+  // _handleDeleteTouchTap(user) {
+  //   let url = `/api/org/${this.props.org}/users/${user.id}`;
 
-  _handleInviteTouchTap() {
-    let url = `/api/org/${this.props.org}/users`;
+  //   deleteData(url)
+  //     .then(function(resp) {
+  //       this.setState({
+  //         users: resp
+  //       });
+  //       this.refs.emailField.setValue("");
+  //     }.bind(this), function(resp) {
+  //       // TODO show error
+  //     }.bind(this));
+  // }
 
-    const email = this.refs.emailField.getValue();
+  // _handleInviteTouchTap() {
+  //   let url = `/api/org/${this.props.org}/users`;
 
-    if (email) {
-      postData(url, {email: email})
-        .then(function(resp) {
-          this.setState({
-            users: resp
-          });
-          this.refs.emailField.setValue("");
-        }.bind(this), function(resp) {
-          // TODO show error
-        }.bind(this));
-      }
-  }
+  //   const email = this.refs.emailField.getValue();
+
+  //   if (email) {
+  //     postData(url, {email: email})
+  //       .then(function(resp) {
+  //         this.setState({
+  //           users: resp
+  //         });
+  //         this.refs.emailField.setValue("");
+  //       }.bind(this), function(resp) {
+  //         // TODO show error
+  //       }.bind(this));
+  //     }
+  // }
 
 }
