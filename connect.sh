b7859e0fe743d79aa525dd2b8b656c74cd0e9893
Tom Wilkie <tom.wilkie@gmail.com>
2015-10-05 14:15:20 +0100

    Use weave config to ensure we talk to the weave proxy.

--- connect.sh
+++ connect.sh
@@ -7,20 +7,21 @@ if [ $# -lt 1 ]; then
 	exit 1
 fi
 
-SSH_ARGS=""
-
 if [ "$1" == "-prod" ]; then
     HOST=ubuntu@$(dig +short docker.cloud.weave.works | sort | head -1)
     SSH_ARGS="-i infrastructure/weave-keypair.pem"
     DOCKER_IP_PORT=$(ssh $SSH_ARGS $HOST weave dns-lookup swarm-master):4567
+    DOCKER_CONFIG="-H=tcp://$DOCKER_IP_PORT"
 else
     HOST=$1
-    DOCKER_IP_PORT=127.0.0.1:12375
+    SSH_ARGS=
+    DOCKER_CONFIG=$(ssh $HOST weave config)
+    DOCKER_IP_PORT="127.0.0.1:12375"
 fi
 shift 1
 
 docker_on() {
-    ssh $SSH_ARGS $HOST -- env DOCKER_HOST=tcp://$DOCKER_IP_PORT docker "$@"
+    ssh $SSH_ARGS $HOST docker $DOCKER_CONFIG "$@"
 }
 
 echo "Starting proxy container..."
@@ -28,13 +29,13 @@ docker_on rm -f $USER-proxy 2>/dev/null || true
 docker_on run -d --name $USER-proxy weaveworks/socksproxy -a scope.weave.works:frontend.weave.local
 
 function finish {
-	echo "Removing proxy container.."
-	docker_on rm -f $USER-proxy
+    echo "Removing proxy container.."
+    docker_on rm -f $USER-proxy
 }
 trap finish EXIT
 
 PROXY_IP=$(ssh $SSH_ARGS $HOST weave dns-lookup $USER-proxy)
 echo 'Please configure your browser for proxy http://localhost:8080/proxy.pac and'
 echo 'export DOCKER_HOST=tcp://127.0.0.1:4567'
-ssh $SSH_ARGS -L8000:$PROXY_IP:8000 -L8080:$PROXY_IP:8080 -L4567:$DOCKER_IP_PORT $HOST -- \
-    env DOCKER_HOST=tcp://$DOCKER_IP_PORT docker attach $USER-proxy
+ssh $SSH_ARGS -L8000:$PROXY_IP:8000 -L8080:$PROXY_IP:8080 -L4567:$DOCKER_IP_PORT $HOST \
+    docker $DOCKER_CONFIG attach $USER-proxy
