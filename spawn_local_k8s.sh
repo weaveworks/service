d36602f22c1ce119d6ef5025a7b0f01368d3f024
Alfonso Acosta <fons@weave.works>
2015-10-29 18:43:59 +0000

    Correct apiserver endpoint for kube2sky

--- spawn_local_k8s.sh
+++ spawn_local_k8s.sh
@@ -20,7 +20,7 @@ stop_container etcd
 # https://github.com/weaveworks/weave/issues/1600
 docker run --name etcd --net=host -d gcr.io/google_containers/etcd:2.0.12 /usr/local/bin/etcd --addr=127.0.0.1:4001 --bind-addr=0.0.0.0:4001 --data-dir=/var/etcd/data
 docker run \
-       --name k8s_master \
+       --name k8s_kubelet \
        --volume=/:/rootfs:ro \
        --volume=/sys:/sys:ro \
        --volume=/dev:/dev \
