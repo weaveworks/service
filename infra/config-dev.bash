968ce41c2e08ace02adc39a4f68ad49a78a85c8c
Peter Bourgon <peter@bourgon.org>
2015-10-30 22:10:19 +0100

    Update config-dev

--- infra/config-dev.bash
+++ infra/config-dev.bash
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-export KUBE_AWS_ZONE="eu-central-1a" # availability zone
-export AWS_S3_REGION="eu-central-1"  # region
+export KUBE_AWS_ZONE="eu-west-1a" # availability zone
+export AWS_S3_REGION="eu-west-1"  # region
 export AWS_S3_BUCKET="weaveworks-scope-kubernetes-dev-$(date | shasum | cut -c 1-7)"
-export AWS_IMAGE="ami-58c1cd45" # http://cloud-images.ubuntu.com/locator/ec2/
+
+# http://cloud-images.ubuntu.com/locator/ec2/
+# export AWS_IMAGE="ami-58c1cd45" # eu-central-1
+export AWS_IMAGE="ami-ef8ab698" # eu-west-1
+
 export MASTER_SIZE="m3.medium"
 export MINION_SIZE="t2.micro"
 export NUM_MINIONS="2"
