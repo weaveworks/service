9bb714a6eadf0551c1259d4a76b935d0b139ac0a
Alfonso Acosta <fons@weave.works>
2015-10-30 01:07:30 +0000

    Move local kubernetes definitions to its own directory

--- spawn_local_k8s.sh
+++ spawn_local_k8s.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+SCRIPT_DIR=$(cd `dirname -- "$0"`; pwd -P)
+
 stop_container() {
     docker stop $1 > /dev/null 2>&1 || true
     docker rm -v $1 > /dev/null 2>&1 || true
@@ -11,6 +13,8 @@ stop_container() {
 for I in $(docker ps -q -f 'name=k8s*'); do
     stop_container $I
 done
+stop_container local_k8s_proxy
+stop_container local_k8s_kubelet
 stop_container etcd
 
 # Spawn new cluster
@@ -20,7 +24,7 @@ stop_container etcd
 # https://github.com/weaveworks/weave/issues/1600
 docker run --name etcd --net=host -d gcr.io/google_containers/etcd:2.0.12 /usr/local/bin/etcd --addr=127.0.0.1:4001 --bind-addr=0.0.0.0:4001 --data-dir=/var/etcd/data
 docker run \
-       --name k8s_kubelet \
+       --name local_k8s_kubelet \
        --volume=/:/rootfs:ro \
        --volume=/sys:/sys:ro \
        --volume=/dev:/dev \
@@ -30,13 +34,13 @@ docker run \
        --net=host \
        --pid=host \
        --privileged=true \
-       -v $PWD/k8s/master.json:/etc/kubernetes/manifests/master.json \
+       -v "$SCRIPT_DIR"/k8s/local/master.json:/etc/kubernetes/manifests/master.json \
        -d \
        gcr.io/google_containers/hyperkube:v1.0.6 \
        /hyperkube kubelet --containerized --hostname-override="127.0.0.1" --address="0.0.0.0" --api-servers=http://localhost:8080 --config=/etc/kubernetes/manifests --cluster-dns=10.0.0.10 --cluster-domain=cluster.local
-docker run --name k8s_proxy -d --net=host --privileged gcr.io/google_containers/hyperkube:v1.0.6 /hyperkube proxy --master=http://127.0.0.1:8080 --v=2
+docker run --name local_k8s_proxy -d --net=host --privileged gcr.io/google_containers/hyperkube:v1.0.6 /hyperkube proxy --master=http://127.0.0.1:8080 --v=2
 # DNS
 # From https://github.com/kubernetes/kubernetes/tree/master/cluster/addons/dns
 # TODO: reuse main etcd instead of spawning another one
 sleep 3 # Let hyperkube boot
-kubectl create -f k8s/skydns
+kubectl create -f "$SCRIPT_DIR"/k8s/local/skydns
