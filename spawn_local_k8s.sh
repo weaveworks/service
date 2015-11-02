46e573cdc101d8c53a355e3fe72c3e86ef2af22d
Alfonso Acosta <fons@weave.works>
2015-11-02 12:11:18 +0000

    Adapt k8s local pki host mount dynamically

--- spawn_local_k8s.sh
+++ spawn_local_k8s.sh
@@ -3,6 +3,8 @@
 set -e
 
 SCRIPT_DIR=$(cd `dirname -- "$0"`; pwd -P)
+LOCAL_K8S_DIR="$SCRIPT_DIR/k8s/local"
+LOCAL_K8S_PKI_DIR="$LOCAL_K8S_DIR/pki"
 
 stop_container() {
     docker stop $1 > /dev/null 2>&1 || true
@@ -18,11 +20,10 @@ stop_container local_k8s_kubelet
 stop_container etcd
 
 # Generate certificates
-
-if ! [ -d "$SCRIPT_DIR/k8s/local/pki" ]; then
+if ! [ -d "$LOCAL_K8S_PKI_DIR" ]; then
     curl -s -L https://raw.githubusercontent.com/kubernetes/kubernetes/master/cluster/saltbase/salt/generate-cert/make-ca-cert.sh -o /tmp/make-ca-cert.sh
     chmod +x /tmp/make-ca-cert.sh
-    CERT_GROUP=`id -g` CERT_DIR="$SCRIPT_DIR"/k8s/local/pki /tmp/make-ca-cert.sh 10.0.0.1 DNS:kubernetes,DNS:kubernetes.default,DNS:kubernetes.default.svc,DNS:kubernetes.default.svc.cluster.local
+    CERT_GROUP=`id -g` CERT_DIR="$LOCAL_K8S_PKI_DIR" /tmp/make-ca-cert.sh 10.0.0.1 DNS:kubernetes,DNS:kubernetes.default,DNS:kubernetes.default.svc,DNS:kubernetes.default.svc.cluster.local
 fi
 
 # Spawn new cluster
@@ -31,6 +32,7 @@ fi
 # weave but ... it doesn't work due to
 # https://github.com/weaveworks/weave/issues/1600
 docker run --name etcd --net=host -d gcr.io/google_containers/etcd:2.0.12 /usr/local/bin/etcd --addr=127.0.0.1:4001 --bind-addr=0.0.0.0:4001 --data-dir=/var/etcd/data
+sed "s%\$PKI_HOST_PATH%${LOCAL_K8S_PKI_DIR}%g" "$LOCAL_K8S_DIR"/master.json.in > "$LOCAL_K8S_DIR"/master.json
 docker run \
        --name local_k8s_kubelet \
        --volume=/:/rootfs:ro \
@@ -42,7 +44,7 @@ docker run \
        --net=host \
        --pid=host \
        --privileged=true \
-       -v "$SCRIPT_DIR"/k8s/local/master.json:/etc/kubernetes/manifests/master.json \
+       -v "$LOCAL_K8S_DIR"/master.json:/etc/kubernetes/manifests/master.json \
        -d \
        2opremio/hyperkube:664d2ef \
        /hyperkube kubelet --containerized --hostname-override="127.0.0.1" --address="0.0.0.0" --api-servers=http://localhost:8080 --config=/etc/kubernetes/manifests --cluster-dns=10.0.0.10 --cluster-domain=cluster.local
@@ -51,5 +53,5 @@ docker run --name local_k8s_proxy -d --net=host --privileged 2opremio/hyperkube:
 # From https://github.com/kubernetes/kubernetes/tree/master/cluster/addons/dns
 # TODO: reuse main etcd instead of spawning another one
 sleep 3 # Let hyperkube boot
-kubectl create -f "$SCRIPT_DIR"/k8s/local/kube-system.json
-kubectl create -f "$SCRIPT_DIR"/k8s/local/skydns
+kubectl create -f "$LOCAL_K8S_DIR"/kube-system.json
+kubectl create -f "$LOCAL_K8S_DIR"/skydns
