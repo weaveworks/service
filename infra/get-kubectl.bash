7cd4dc1e56bd3bb56bcdea9b8711a4b29dc1acde
Peter Bourgon <peter@bourgon.org>
2015-11-02 09:24:13 +0100

    A much more efficient get-kubectl.bash

--- infra/get-kubectl.bash
+++ infra/get-kubectl.bash
@@ -3,21 +3,8 @@
 VERSION=v1.0.7
 OS=$(uname | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m) ; ARCH=${ARCH/x86_64/amd64}
-KUBECTL=kubernetes/platforms/${OS}/${ARCH}/kubectl
 
-if [ ! -f kubernetes.tar.gz ]
-then
-	wget -q --show-progress https://github.com/kubernetes/kubernetes/releases/download/${VERSION}/kubernetes.tar.gz
-fi
-
-if [ ! -f ${KUBECTL} ]
-then
-	tar zxvf kubernetes.tar.gz ${KUBECTL}
-fi
-
-if [ ! -f kubectl ]
-then
-	cp ${KUBECTL} .
-fi
+wget -q --show-progress -O kubectl https://storage.googleapis.com/kubernetes-release/release/${VERSION}/bin/${OS}/${ARCH}/kubectl
 
 echo "You may want to put kubectl to somewhere in your PATH"
+
