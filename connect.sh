6cf67e1eb7c38e1f9b7ea05092f400a7e1ab1c3d
Alfonso Acosta <fons@weave.works>
2015-11-10 17:30:30 +0000

    k8s: port connect.sh <host> to kubernetes

--- connect.sh
+++ connect.sh
@@ -3,9 +3,20 @@
 set -eu
 
 if [ $# -lt 1 ]; then
-	echo "Usage: $0 (-prod|<host>)"
+	echo "Usage: $0 (-prod|-dev|<host>)"
 	exit 1
 fi
+
+kubectl_on() {
+    ssh $SSH_ARGS $HOST kubectl "$@"
+}
+
+function k8s_finish {
+    echo "Removing proxy service.."
+    kubectl_on delete rc fons-proxy
+    kubectl_on delete service fons-proxy
+}
+
 
 LOCAL_PORT=4567
 if [ "$1" == "-prod" ]; then
@@ -21,16 +32,35 @@ elif [ "$1" == "-dev" ]; then
     DOCKER_IP_PORT=$(ssh $SSH_ARGS $HOST weave dns-lookup swarm-master):4567
     DOCKER_CONFIG="-H=tcp://$DOCKER_IP_PORT"
 else
+    # Use local k8s cluster
+    # TODO: extend it to -prod and -dev
     HOST=$1
+    shift 1
     SSH_ARGS=
-    DOCKER_CONFIG=$(ssh $HOST weave config)
-    DOCKER_IP_PORT="127.0.0.1:12375"
+    LOCAL_API_SERVER_PORT=9090
+    # TODO: contacting the API server will be different in non-local k8s. We will
+    # probably want to use 'kubectl proxy' to handle authentication and so on ...
+    API_SERVER_PORT=8080
 
-    echo "Weave exposing..."
-    status=$(ssh $SSH_ARGS $HOST weave ps weave:expose | awk '{print $3}' 2>/dev/null)
-    if [ -z "$status" ]; then
-        ssh $SSH_ARGS $HOST weave expose
-    fi
+    k8s_finish 2>/dev/null || true
+    echo "Starting proxy service..."
+
+    # TODO: It probably makes more sense to use something like https://github.com/robszumski/k8s-service-proxy instead of
+    # weaveworks/socksproxy
+    kubectl_on run $USER-proxy --image=weaveworks/socksproxy --command -- /proxy -a scope.weave.works:frontend
+    trap k8s_finish EXIT
+
+    # Override needed because 'kubectl expose' doesn't allow providing multiple ports
+    PROXY_SERVICE_PORTS=$(printf %q '{ "apiVersion": "v1", "spec": { "ports": [{"name": "socks", "port":8080}, {"name": "pac", "port":8000}] } }')
+    kubectl_on expose rc $USER-proxy --port=8080 --overrides="$PROXY_SERVICE_PORTS"
+    PROXY_IP=$(kubectl_on get service $USER-proxy -o template --template='{{.spec.clusterIP}}')
+    PROXY_NAME_TEMPLATE=$(printf %q '{{ (index .items  0).metadata.name }}')
+    PROXY_POD=$(kubectl_on get pod --selector=run=$USER-proxy -o template --template="$PROXY_NAME_TEMPLATE")
+    echo "Please configure your browser for proxy http://localhost:8080/proxy.pac and"
+    echo "provide '-s localhost:$LOCAL_API_SERVER_PORT' to kubectl"
+    ssh $SSH_ARGS -L8000:$PROXY_IP:8000 -L8080:$PROXY_IP:8080 -L$LOCAL_API_SERVER_PORT:localhost:$API_SERVER_PORT $HOST \
+	kubectl logs -f $PROXY_POD -c $USER-proxy
+    exit 0
 fi
 shift 1
 
