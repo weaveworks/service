32fce3c0e75d906fe95b680987e2530107035d74
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-02-29 21:28:51 +0000

    Do AZs properly

--- infra/terraform/databases/main.tf
+++ infra/terraform/databases/main.tf
@@ -4,6 +4,7 @@ variable "postgres_version" {
 
 variable "cluster" {}
 variable "rds_sg_id" {}
+variable "rds_vpc_id" {}
 
 variable "users_db_password" {}
 variable "app-mapper_db_password" {}
@@ -15,38 +16,63 @@ variable "aws_region" {
     default = "us-east-1"
 }
 
+variable "az1" {}
+variable "az2" {}
+
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
-    region = "${var.aws_region}"
+    region     = "${var.aws_region}"
 }
 
 resource "aws_db_instance" "users_database" {
-    identifier = "users-database-${var.cluster}"
-    allocated_storage = 10
-    multi_az = false
-    engine = "postgres"
-    engine_version = "${var.postgres_version}"
-    instance_class = "db.t2.small"
-    username = "postgres"
-    password = "${var.users_db_password}"
-    vpc_security_group_ids = ["${var.rds_sg_id}"]
+    identifier                = "users-database-${var.cluster}"
+    allocated_storage         = 10
+    multi_az                  = true
+    engine                    = "postgres"
+    engine_version            = "${var.postgres_version}"
+    instance_class            = "db.t2.small"
+    username                  = "postgres"
+    password                  = "${var.users_db_password}"
+    vpc_security_group_ids    = ["${var.rds_sg_id}"]
+    db_subnet_group_name      = "${aws_db_subnet_group.default_rds_subnet.name}"
     final_snapshot_identifier = "users-final"
-    backup_retention_period = 5
-    apply_immediately = true
+    backup_retention_period   = 5
+    apply_immediately         = true
 }
 
 resource "aws_db_instance" "app_mapper_database" {
-    identifier = "app-mapper-database-${var.cluster}"
-    allocated_storage = 10
-    multi_az = false
-    engine = "postgres"
-    engine_version = "${var.postgres_version}"
-    instance_class = "db.t2.small"
-    username = "postgres"
-    password = "${var.app-mapper_db_password}"
-    vpc_security_group_ids = ["${var.rds_sg_id}"]
+    identifier                = "app-mapper-database-${var.cluster}"
+    allocated_storage         = 10
+    multi_az                  = true
+    engine                    = "postgres"
+    engine_version            = "${var.postgres_version}"
+    instance_class            = "db.t2.small"
+    username                  = "postgres"
+    password                  = "${var.app-mapper_db_password}"
+    vpc_security_group_ids    = ["${var.rds_sg_id}"]
+    db_subnet_group_name      = "${aws_db_subnet_group.default_rds_subnet.name}"
     final_snapshot_identifier = "app-mapper-final"
-    backup_retention_period = 5
-    apply_immediately = true
+    backup_retention_period   = 5
+    apply_immediately         = true
+}
+
+resource "aws_subnet" "rds_subnet1" {
+    vpc_id                  = "${var.rds_vpc_id}"
+    cidr_block              = "172.20.251.0/24"
+    map_public_ip_on_launch = false
+    availability_zone       = "${var.az1}"
+}
+
+resource "aws_subnet" "rds_subnet2" {
+    vpc_id                  = "${var.rds_vpc_id}"
+    cidr_block              = "172.20.252.0/24"
+    map_public_ip_on_launch = false
+    availability_zone       = "${var.az2}"
+}
+
+resource "aws_db_subnet_group" "default_rds_subnet" {
+    name        = "db-subnet-group-${var.cluster}"
+    description =  "Subnet 1 for Postgres RDS instances"
+    subnet_ids  = ["${aws_subnet.rds_subnet1.id}", "${aws_subnet.rds_subnet2.id}"]
 }
--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -4,6 +4,11 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+module "az" {
+    source = "github.com/errordeveloper/tf_aws_availability_zones"
+    region = "eu-central-1"
+}
+
 module "kubernetes-anywhere-aws-ec2" {
     source         = "github.com/weaveworks/weave-kubernetes-anywhere/examples/aws-ec2-terraform"
     aws_access_key = "AKIAJ7FURWZ4EFA4RUDA"
@@ -19,8 +24,12 @@ module "databases" {
     aws_secret_key = "60DRnBPPbLn7drqhLVBf4Bkv4c5r8iPLyEpkSxJy"
     aws_region     = "eu-central-1"
 
+    az1            = "${module.az.primary}"
+    az2            = "${module.az.secondary}"
+
     cluster        = "devc"
     rds_sg_id      = "${module.kubernetes-anywhere-aws-ec2.kubernetes-main-sg-id}"
+    rds_vpc_id     = "${module.kubernetes-anywhere-aws-ec2.kubernetes-vpc-id}"
     users_db_password = "29fcf745896ac1f83c15ea03a22afcc2e4851048"
     app-mapper_db_password = "c798bb333cf5d9c211921e0682c1fcdb507d1e85"
 }
