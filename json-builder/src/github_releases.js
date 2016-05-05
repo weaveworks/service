0d7927a0926ce219be49f087c251385513bc9f5f
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-05-05 14:52:02 +0100

    Polling Github releases works better, also saves one callback

--- json-builder/src/github_releases.js
+++ json-builder/src/github_releases.js
@@ -1,14 +1,26 @@
 // this package guards agains rate-limitting and has local cache
 var ghrequest = require('ghrequest');
 
-var req = {
-  url: '/repos/weaveworks/scope/releases',
-  headers: { 'User-Agent': 'My Cool App' }
-};
+var latest;
 
-exports.get_latest_scope_release = function(log, callback) {
+function update() {
+
+  var req = {
+    url: '/repos/weaveworks/scope/releases',
+    headers: { 'User-Agent': 'My Cool App' }
+  };
+
   ghrequest(req, function(err, res, body) {
     if (err) throw err;
-    callback(body[1].tag_name);
+    latest = body[1].tag_name;
   });
-};
+}
+
+exports.get_latest_scope_release = function() {
+  return latest;
+}
+
+exports.poll = function() {
+  update();
+  setInterval(update, 900000) // 15min
+}
