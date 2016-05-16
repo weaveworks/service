8cdb09ee361938685f3bda0a7d46b153d7b895c0
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-05-16 13:02:07 +0100

    Code cleanup, fix SIGINT handling

--- connect.go
+++ connect.go
@@ -38,29 +38,39 @@ func (endpoint *Endpoint) String() string {
 }
 
 type SSHtunnel struct {
-	Local  *Endpoint
-	Server *Endpoint
-	Remote *Endpoint
-
-	Config *ssh.ClientConfig
+	Local    *Endpoint
+	Server   *Endpoint
+	Remote   *Endpoint
+	Config   *ssh.ClientConfig
+	listener net.Listener
 }
 
-func (tunnel *SSHtunnel) Start(ready chan bool) error {
+func (tunnel *SSHtunnel) Start(ready chan bool) (err error) {
 	log.Println("Started processing tunnel traffic")
-	listener, err := net.Listen("tcp", tunnel.Local.String())
+	tunnel.listener, err = net.Listen("tcp", tunnel.Local.String())
 	if err != nil {
-		return err
+		return
 	}
-	defer listener.Close()
+
 	ready <- true
 
 	for {
-		conn, err := listener.Accept()
+		conn, err := tunnel.listener.Accept()
 		if err != nil {
 			return err
 		}
 		go tunnel.forward(conn)
 	}
+}
+
+func (tunnel *SSHtunnel) Stop() (err error) {
+	err = tunnel.listener.Close()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("Tunnel closed")
+	return
 }
 
 func (tunnel *SSHtunnel) forward(localConn net.Conn) {
@@ -80,7 +90,7 @@ func (tunnel *SSHtunnel) forward(localConn net.Conn) {
 	copyConn := func(writer, reader net.Conn) {
 		s, err := io.Copy(writer, reader)
 		if err != nil {
-			log.Println("io.Copy error: %s", err)
+			log.Printf("I/O error: %s", err)
 		}
 		log.Printf("Copied %d bytes for connection %v", s, reader)
 	}
@@ -89,13 +99,12 @@ func (tunnel *SSHtunnel) forward(localConn net.Conn) {
 	go copyConn(remoteConn, localConn)
 }
 
-func parsePrivateKey(keyPath string) (ssh.Signer, error) {
-	buff, _ := ioutil.ReadFile(keyPath)
-	return ssh.ParsePrivateKey(buff)
-}
-
 func makeSshConfig(user, privateKeyPath string) (*ssh.ClientConfig, error) {
-	key, err := parsePrivateKey(privateKeyPath)
+	buff, err := ioutil.ReadFile(privateKeyPath)
+	if err != nil {
+		return nil, err
+	}
+	key, err := ssh.ParsePrivateKey(buff)
 	if err != nil {
 		return nil, err
 	}
@@ -110,14 +119,140 @@ func makeSshConfig(user, privateKeyPath string) (*ssh.ClientConfig, error) {
 	return &config, nil
 }
 
-func getNodes() (nodes []string, err error) {
+type KubernetesClient struct {
+	podName        string
+	deploymentName string
+	c              *unversioned.Client
+	ec             *unversioned.ExtensionsClient
+	ConfigPath     string
+	UserName       string
+}
+
+func (kube *KubernetesClient) CreateProxy(stop <-chan struct{}) (err error) {
+	log.Println("Creating Kubernetes client...")
+
+	loadingRules := &clientcmd.ClientConfigLoadingRules{ExplicitPath: kube.ConfigPath}
+	configOverrides := &clientcmd.ConfigOverrides{}
+	kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, configOverrides)
+
+	config, err := kubeconfig.ClientConfig()
+	if err != nil {
+		return
+	}
+
+	kube.c, err = unversioned.New(config)
+	if err != nil {
+		return
+	}
+
+	kube.ec, err = unversioned.NewExtensions(config)
+	if err != nil {
+		return
+	}
+
+	kube.deploymentName = "socksproxy-" + kube.UserName
+	l := map[string]string{"name": kube.deploymentName}
+
+	log.Println("Creating socksproxy deployment...")
+	zero := int64(0)
+	d := &extensions.Deployment{
+		ObjectMeta: api.ObjectMeta{Name: kube.deploymentName},
+		Spec: extensions.DeploymentSpec{
+			Replicas: 1,
+			Selector: &unversionedapi.LabelSelector{MatchLabels: l},
+			Template: api.PodTemplateSpec{
+				ObjectMeta: api.ObjectMeta{Labels: l},
+				Spec: api.PodSpec{
+					TerminationGracePeriodSeconds: &zero,
+					Containers: []api.Container{
+						{
+							Name:  "socksproxy",
+							Image: "weaveworks/socksproxy:latest",
+						},
+					},
+				},
+			},
+		},
+	}
+	_, err = kube.ec.Deployments(api.NamespaceDefault).Create(d)
+	if err != nil {
+		if errors.IsAlreadyExists(err) {
+			log.Printf("Deployment of socksproxy with name %q already exists - cleaning up...", kube.deploymentName)
+			err = kube.ec.Deployments(api.NamespaceDefault).Delete(kube.deploymentName, &api.DeleteOptions{})
+			if err != nil {
+				return
+			}
+			log.Println("Creating socksproxy deployment...")
+			_, err = kube.ec.Deployments(api.NamespaceDefault).Create(d)
+			if err != nil {
+				return
+			}
+		} else {
+			return
+		}
+	}
+
+	pods, err := kube.c.Pods(api.NamespaceDefault).List(api.ListOptions{LabelSelector: labels.Set(l).AsSelector()})
+	if err != nil {
+		return
+	}
+
+	kube.podName = pods.Items[0].ObjectMeta.Name
+
+	pod, err := kube.c.Pods(api.NamespaceDefault).Get(kube.podName)
+	if err != nil {
+		return
+	}
+
+	for pod.Status.Phase != api.PodRunning {
+		log.Println("Waiting for pod %q...", kube.podName)
+		time.Sleep(2 * time.Second)
+		pod, err = kube.c.Pods(api.NamespaceDefault).Get(kube.podName)
+		if err != nil {
+			return
+		}
+	}
+
+	log.Printf("Creating port forwarder for pod %q...", kube.podName)
+	req := kube.c.RESTClient.Post().Resource("pods").Namespace(api.NamespaceDefault).Name(kube.podName).SubResource("portforward")
+
+	ports := []string{"8000", "8080"}
+
+	dialer, err := remotecommand.NewExecutor(config, "POST", req.URL())
+	if err != nil {
+		return
+	}
+
+	fw, err := portforward.New(dialer, ports, stop, os.Stdout, os.Stderr)
+	if err != nil {
+		return
+	}
+
+	err = fw.ForwardPorts()
+	if err != nil {
+		return
+	}
+	log.Println("Port forwarder terminated")
+	return
+}
+
+func (kube *KubernetesClient) DeleteProxy() (err error) {
+	log.Println("Deleting deployment of socksproxy...")
+	err = kube.ec.Deployments(api.NamespaceDefault).Delete(kube.deploymentName, &api.DeleteOptions{})
+	if err != nil {
+		return
+	}
+	return
+}
+
+func getEc2Nodes(cluster string) (nodes []string, err error) {
 	svc := ec2.New(session.New(), &aws.Config{Region: aws.String("us-east-1")})
 	params := &ec2.DescribeInstancesInput{
 		Filters: []*ec2.Filter{
 			{
 				Name: aws.String("tag:KubernetesCluster"),
 				Values: []*string{
-					aws.String("kubernetes-devz"),
+					aws.String(cluster),
 				},
 			},
 			{
@@ -151,38 +286,23 @@ func getNodes() (nodes []string, err error) {
 
 func main() {
 
-	//TODO: make it work properly
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, os.Interrupt)
 	defer signal.Stop(signals)
 
-	stop := make(chan struct{}, 1)
-	go func() {
-		<-signals
-		close(stop)
-	}()
+	client := &KubernetesClient{ConfigPath: "infra/dev/kubeconfig", UserName: "ilya"}
 
-	nodes, err := getNodes()
+	tunnelReady, tunnelExit, stopForwarder := make(chan bool, 1), make(chan bool, 1), make(chan struct{}, 1)
+
+	nodes, err := getEc2Nodes("kubernetes-devz")
 	if err != nil {
 		log.Fatalln(err)
 	}
+
 	log.Println("nodes:", nodes)
+
 	rand.Seed(time.Now().UTC().UnixNano())
-
-	localEndpoint := &Endpoint{
-		Host: "localhost",
-		Port: 6443,
-	}
-
-	serverEndpoint := &Endpoint{
-		Host: nodes[rand.Intn(len(nodes))],
-		Port: 22,
-	}
-
-	remoteEndpoint := &Endpoint{
-		Host: "10.16.0.1",
-		Port: 443,
-	}
+	randomNode := nodes[rand.Intn(len(nodes))]
 
 	config, err := makeSshConfig("ubuntu", "infra/dev/ec2_weaveworks.kubernetes-anywhere.us-east-1.pem")
 	if err != nil {
@@ -193,14 +313,29 @@ func main() {
 
 	tunnel := &SSHtunnel{
 		Config: config,
-		Local:  localEndpoint,
-		Server: serverEndpoint,
-		Remote: remoteEndpoint,
+		Local:  &Endpoint{Host: "localhost", Port: 6443},
+		Server: &Endpoint{Host: randomNode, Port: 22},
+		Remote: &Endpoint{Host: "10.16.0.1", Port: 443},
 	}
 
 	log.Printf("Created SSH tunnel configuration: %#v\n", tunnel)
 
-	tunnelReady, tunnelExit := make(chan bool), make(chan bool)
+	go func() {
+		<-signals
+		log.Println("Caught SIGINT...")
+		close(stopForwarder)
+		err := client.DeleteProxy()
+		if err != nil {
+			log.Println(err)
+		}
+		log.Println("Waiting...")
+		time.Sleep(5 * time.Second)
+		err = tunnel.Stop()
+		if err != nil {
+			log.Println(err)
+		}
+
+	}()
 
 	go func() {
 		err := tunnel.Start(tunnelReady)
@@ -212,131 +347,12 @@ func main() {
 
 	<-tunnelReady
 
-	log.Println("You can use `kubectl --kubeconfig=infra/dev/kubeconfig` to interact with the system")
+	log.Printf("You can use `kubectl --kubeconfig=%q` to interact with the system", client.ConfigPath)
 
 	go func() {
-		log.Println("Creating Kubernetes client...")
-
-		loadingRules := &clientcmd.ClientConfigLoadingRules{ExplicitPath: "infra/dev/kubeconfig"}
-		configOverrides := &clientcmd.ConfigOverrides{}
-		kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, configOverrides)
-
-		config, err := kubeconfig.ClientConfig()
+		err := client.CreateProxy(stopForwarder)
 		if err != nil {
 			log.Println(err)
-			return
-		}
-
-		c, err := unversioned.New(config)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		ec, err := unversioned.NewExtensions(config)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		l := map[string]string{"name": "socksproxy-ilya"}
-
-		log.Println("Creating socksproxy deployment...")
-		zero := int64(0)
-		d := &extensions.Deployment{
-			ObjectMeta: api.ObjectMeta{
-				Name: "socksproxy-ilya",
-			},
-			Spec: extensions.DeploymentSpec{
-				Replicas: 1,
-				Selector: &unversionedapi.LabelSelector{MatchLabels: l},
-				Template: api.PodTemplateSpec{
-					ObjectMeta: api.ObjectMeta{Labels: l},
-					Spec: api.PodSpec{
-						TerminationGracePeriodSeconds: &zero,
-						Containers: []api.Container{
-							{
-								Name:  "socksproxy",
-								Image: "weaveworks/socksproxy:latest",
-							},
-						},
-					},
-				},
-			},
-		}
-		_, err = ec.Deployments(api.NamespaceDefault).Create(d)
-		if err != nil {
-			log.Println(err)
-			if errors.IsAlreadyExists(err) {
-				log.Println("Deployment of socksproxy with name \"socksproxy-ilya\" already exists - cleaning up...")
-				err = ec.Deployments(api.NamespaceDefault).Delete("socksproxy-ilya", &api.DeleteOptions{})
-				if err != nil {
-					log.Println(err)
-					return
-				}
-				log.Println("Creating socksproxy deployment...")
-				_, err = ec.Deployments(api.NamespaceDefault).Create(d)
-				if err != nil {
-					log.Println(err)
-					return
-				}
-			} else {
-				return
-			}
-			go func() {
-				<-signals
-				log.Println("Deleting deployment of socksproxy...")
-				err = ec.Deployments(api.NamespaceDefault).Delete("socksproxy-ilya", &api.DeleteOptions{})
-				if err != nil {
-					log.Println(err)
-					return
-				}
-			}()
-		}
-
-		pods, err := c.Pods(api.NamespaceDefault).List(api.ListOptions{LabelSelector: labels.Set(l).AsSelector()})
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		podName := pods.Items[0].ObjectMeta.Name
-
-		pod, err := c.Pods(api.NamespaceDefault).Get(podName)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		for pod.Status.Phase != api.PodRunning {
-			pod, err = c.Pods(api.NamespaceDefault).Get(podName)
-			if err != nil {
-				log.Println(err)
-				return
-			}
-			time.Sleep(2 * time.Second)
-		}
-
-		req := c.RESTClient.Post().Resource("pods").Namespace(api.NamespaceDefault).Name(pod.Name).SubResource("portforward")
-
-		ports := []string{"8000", "8080"}
-
-		dialer, err := remotecommand.NewExecutor(config, "POST", req.URL())
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		fw, err := portforward.New(dialer, ports, stop, os.Stdout, os.Stderr)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		err = fw.ForwardPorts()
-		if err != nil {
-			log.Println(err)
-			return
 		}
 	}()
 
