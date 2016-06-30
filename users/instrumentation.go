75dc9a80b80ca2544bb81319d033f58b0c53ea79
Tom Wilkie <tom.wilkie@gmail.com>
2016-06-30 10:11:06 +0100

    Update authfe and users instrumentation inline with best practices

--- users/instrumentation.go
+++ users/instrumentation.go
@@ -9,8 +9,8 @@ import (
 var (
 	requestDuration = prometheus.NewSummaryVec(prometheus.SummaryOpts{
 		Namespace: "scope",
-		Name:      "request_duration_nanoseconds",
-		Help:      "Time spent serving HTTP requests.",
+		Name:      "request_duration_seconds",
+		Help:      "Time spent (in seconds) serving HTTP requests.",
 	}, []string{"method", "route", "status_code", "ws"})
 )
 
