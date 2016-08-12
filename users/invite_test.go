3a8af11bff9e6a5fc977f38e4ab1a99ae81825f4
Matthias Radestock <matthias.radestock@gmail.com>
2016-08-12 10:49:59 +0100

    refactor: add helpers for invite tests

--- users/invite_test.go
+++ users/invite_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -11,6 +12,38 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// requestOrgAs sends a request for an organization as the given user,
+// and asserts that the response has the given status. It returns the
+// response body parsed as a JSON object.
+func requestOrgAs(t *testing.T, user *user, method string, org string, email string, body io.Reader, expectedStatus int) map[string]interface{} {
+	w := httptest.NewRecorder()
+	path := "/api/users/org/" + org + "/users"
+	if email != "" {
+		path = path + "/" + email
+	}
+	r := requestAs(t, user, method, path, body)
+	app.ServeHTTP(w, r)
+	assert.Equal(t, expectedStatus, w.Code)
+	if w.Body.Len() == 0 {
+		return nil
+	}
+	responseBody := map[string]interface{}{}
+	assert.NoError(t, json.Unmarshal(w.Body.Bytes(), &responseBody))
+	return responseBody
+}
+
+func requestInvite(t *testing.T, user *user, org *organization, email string, expectedStatus int) map[string]interface{} {
+	return requestOrgAs(t, user, "POST", org.ExternalID, "", jsonBody{"email": email}.Reader(t), expectedStatus)
+}
+
+func assertEmailSent(t *testing.T, to string, contains string) {
+	if assert.Len(t, sentEmails, 1) {
+		assert.Equal(t, []string{to}, sentEmails[0].To)
+		assert.Contains(t, string(sentEmails[0].Text), contains)
+		assert.Contains(t, string(sentEmails[0].HTML), contains)
+	}
+}
+
 func Test_InviteNonExistentUser(t *testing.T) {
 	setup(t)
 	defer cleanup(t)
@@ -18,13 +51,7 @@ func Test_InviteNonExistentUser(t *testing.T) {
 	user, org := getOrg(t)
 	franEmail := "fran@weave.works"
 
-	w := httptest.NewRecorder()
-	r := requestAs(t, user, "POST", "/api/users/org/"+org.ExternalID+"/users", jsonBody{"email": franEmail}.Reader(t))
-
-	app.ServeHTTP(w, r)
-	assert.Equal(t, http.StatusOK, w.Code)
-	body := map[string]interface{}{}
-	assert.NoError(t, json.Unmarshal(w.Body.Bytes(), &body))
+	body := requestInvite(t, user, org, franEmail, http.StatusOK)
 	assert.Equal(t, map[string]interface{}{
 		"mailSent": true,
 		"email":    franEmail,
@@ -36,11 +63,7 @@ func Test_InviteNonExistentUser(t *testing.T) {
 	assert.Equal(t, org.ID, fran.Organizations[0].ID)
 	assert.Equal(t, fran.Email, franEmail)
 
-	if assert.Len(t, sentEmails, 1) {
-		assert.Equal(t, []string{franEmail}, sentEmails[0].To)
-		assert.Contains(t, string(sentEmails[0].Text), "has invited you")
-		assert.Contains(t, string(sentEmails[0].HTML), "has invited you")
-	}
+	assertEmailSent(t, franEmail, "has invited you")
 }
 
 func Test_InviteExistingUser(t *testing.T) {
@@ -50,13 +73,7 @@ func Test_InviteExistingUser(t *testing.T) {
 	user, org := getOrg(t)
 	fran := getApprovedUser(t)
 
-	w := httptest.NewRecorder()
-	r := requestAs(t, user, "POST", "/api/users/org/"+org.ExternalID+"/users", jsonBody{"email": fran.Email}.Reader(t))
-
-	app.ServeHTTP(w, r)
-	assert.Equal(t, http.StatusOK, w.Code)
-	body := map[string]interface{}{}
-	assert.NoError(t, json.Unmarshal(w.Body.Bytes(), &body))
+	body := requestInvite(t, user, org, fran.Email, http.StatusOK)
 	assert.Equal(t, map[string]interface{}{
 		"mailSent": true,
 		"email":    fran.Email,
@@ -67,11 +84,7 @@ func Test_InviteExistingUser(t *testing.T) {
 	require.Len(t, fran.Organizations, 1)
 	assert.Equal(t, org.ID, fran.Organizations[0].ID)
 
-	if assert.Len(t, sentEmails, 1) {
-		assert.Equal(t, []string{fran.Email}, sentEmails[0].To)
-		assert.Contains(t, string(sentEmails[0].Text), "has granted you access")
-		assert.Contains(t, string(sentEmails[0].HTML), "has granted you access")
-	}
+	assertEmailSent(t, fran.Email, "has granted you access")
 }
 
 func Test_Invite_WithInvalidJSON(t *testing.T) {
@@ -80,11 +93,8 @@ func Test_Invite_WithInvalidJSON(t *testing.T) {
 
 	user, org := getOrg(t)
 
-	w := httptest.NewRecorder()
-	r := requestAs(t, user, "POST", "/api/users/org/"+org.ExternalID+"/users", strings.NewReader(`garbage`))
+	requestOrgAs(t, user, "POST", org.ExternalID, "", strings.NewReader(`garbage`), http.StatusBadRequest)
 
-	app.ServeHTTP(w, r)
-	assert.Equal(t, http.StatusBadRequest, w.Code)
 	assert.Len(t, sentEmails, 0)
 }
 
@@ -94,12 +104,14 @@ func Test_Invite_WithBlankEmail(t *testing.T) {
 
 	user, org := getOrg(t)
 
-	w := httptest.NewRecorder()
-	r := requestAs(t, user, "POST", "/api/users/org/"+org.ExternalID+"/users", jsonBody{"email": ""}.Reader(t))
+	body := requestInvite(t, user, org, "", http.StatusBadRequest)
+	assert.Equal(t, map[string]interface{}{
+		"errors": []interface{}{
+			map[string]interface{}{
+				"message": "Email cannot be blank"},
+		},
+	}, body)
 
-	app.ServeHTTP(w, r)
-	assert.Equal(t, http.StatusBadRequest, w.Code)
-	assert.Contains(t, w.Body.String(), "Email cannot be blank")
 	assert.Len(t, sentEmails, 0)
 }
 
@@ -117,22 +129,14 @@ func Test_Invite_UserAlreadyInSameOrganization(t *testing.T) {
 	assert.Equal(t, org.ID, fran.Organizations[0].ID)
 	assert.Equal(t, created, false)
 
-	w := httptest.NewRecorder()
-	r := requestAs(t, user, "POST", "/api/users/org/"+org.ExternalID+"/users", jsonBody{"email": fran.Email}.Reader(t))
-
-	app.ServeHTTP(w, r)
-	assert.Equal(t, http.StatusOK, w.Code)
+	requestInvite(t, user, org, fran.Email, http.StatusOK)
 
 	fran, err = storage.FindUserByEmail(fran.Email)
 	require.NoError(t, err)
 	require.Len(t, fran.Organizations, 1)
 	assert.Equal(t, org.ID, fran.Organizations[0].ID)
 
-	if assert.Len(t, sentEmails, 1) {
-		assert.Equal(t, []string{"fran@weave.works"}, sentEmails[0].To)
-		assert.Contains(t, string(sentEmails[0].Text), "has granted you access")
-		assert.Contains(t, string(sentEmails[0].HTML), "has granted you access")
-	}
+	assertEmailSent(t, fran.Email, "has granted you access")
 }
 
 func Test_Invite_UserToAnOrgIDontOwn(t *testing.T) {
@@ -143,11 +147,7 @@ func Test_Invite_UserToAnOrgIDontOwn(t *testing.T) {
 	otherUser := getApprovedUser(t)
 	_, otherOrg := getOrg(t)
 
-	w := httptest.NewRecorder()
-	r := requestAs(t, user, "POST", "/api/users/org/"+otherOrg.ExternalID+"/users", jsonBody{"email": otherUser.Email}.Reader(t))
-
-	app.ServeHTTP(w, r)
-	assert.Equal(t, http.StatusForbidden, w.Code)
+	requestInvite(t, user, otherOrg, otherUser.Email, http.StatusForbidden)
 
 	otherUser, err := storage.FindUserByID(otherUser.ID)
 	require.NoError(t, err)
@@ -164,22 +164,14 @@ func Test_Invite_UserNotApproved(t *testing.T) {
 	fran, err := storage.CreateUser("fran@weave.works")
 	require.NoError(t, err)
 
-	w := httptest.NewRecorder()
-	r := requestAs(t, user, "POST", "/api/users/org/"+org.ExternalID+"/users", jsonBody{"email": fran.Email}.Reader(t))
-
-	app.ServeHTTP(w, r)
-	assert.Equal(t, http.StatusOK, w.Code)
+	requestInvite(t, user, org, fran.Email, http.StatusOK)
 
 	fran, err = storage.FindUserByEmail(fran.Email)
 	require.NoError(t, err)
 	require.Len(t, fran.Organizations, 1)
 	assert.Equal(t, org.ID, fran.Organizations[0].ID)
 
-	if assert.Len(t, sentEmails, 1) {
-		assert.Equal(t, []string{fran.Email}, sentEmails[0].To)
-		assert.Contains(t, string(sentEmails[0].Text), "has granted you access")
-		assert.Contains(t, string(sentEmails[0].HTML), "has granted you access")
-	}
+	assertEmailSent(t, fran.Email, "has granted you access")
 }
 
 func Test_Invite_UserInDifferentOrganization(t *testing.T) {
@@ -189,12 +181,7 @@ func Test_Invite_UserInDifferentOrganization(t *testing.T) {
 	user, org := getOrg(t)
 	fran, _ := getOrg(t)
 
-	w := httptest.NewRecorder()
-	r := requestAs(t, user, "POST", "/api/users/org/"+org.ExternalID+"/users", jsonBody{"email": fran.Email}.Reader(t))
-
-	app.ServeHTTP(w, r)
-
-	assert.Equal(t, http.StatusOK, w.Code)
+	requestInvite(t, user, org, fran.Email, http.StatusOK)
 
 	fran, err := storage.FindUserByEmail(fran.Email)
 	require.NoError(t, err)
@@ -202,11 +189,7 @@ func Test_Invite_UserInDifferentOrganization(t *testing.T) {
 	orgIDs := []string{fran.Organizations[0].ID, fran.Organizations[1].ID}
 	assert.Contains(t, orgIDs, org.ID)
 
-	if assert.Len(t, sentEmails, 1) {
-		assert.Equal(t, []string{fran.Email}, sentEmails[0].To)
-		assert.Contains(t, string(sentEmails[0].Text), "has granted you access")
-		assert.Contains(t, string(sentEmails[0].HTML), "has granted you access")
-	}
+	assertEmailSent(t, fran.Email, "has granted you access")
 }
 
 func Test_Invite_RemoveOtherUsersAccess(t *testing.T) {
@@ -219,30 +202,21 @@ func Test_Invite_RemoveOtherUsersAccess(t *testing.T) {
 	require.NoError(t, err)
 	require.Len(t, otherUser.Organizations, 1)
 
-	{
-		w := httptest.NewRecorder()
-		r := requestAs(t, user, "DELETE", "/api/users/org/"+org.ExternalID+"/users/"+otherUser.Email, nil)
-
-		app.ServeHTTP(w, r)
-		assert.Equal(t, http.StatusNoContent, w.Code)
-	}
+	requestOrgAs(t, user, "DELETE", org.ExternalID, otherUser.Email, nil, http.StatusNoContent)
 
 	otherUser, err = storage.FindUserByID(otherUser.ID)
 	require.NoError(t, err)
 	require.Len(t, otherUser.Organizations, 0)
 
-	{
-		w := httptest.NewRecorder()
-		r := requestAs(t, user, "GET", "/api/users/org/"+org.ExternalID+"/users", nil)
-
-		app.ServeHTTP(w, r)
-		assert.Equal(t, http.StatusOK, w.Code)
-		body := map[string]interface{}{}
-		assert.NoError(t, json.Unmarshal(w.Body.Bytes(), &body))
-		assert.Equal(t, map[string]interface{}{
-			"users": []interface{}{map[string]interface{}{"email": user.Email, "self": true}},
-		}, body)
-	}
+	body := requestOrgAs(t, user, "GET", org.ExternalID, "", nil, http.StatusOK)
+	assert.Equal(t, map[string]interface{}{
+		"users": []interface{}{
+			map[string]interface{}{
+				"email": user.Email,
+				"self":  true,
+			},
+		},
+	}, body)
 }
 
 func Test_Invite_RemoveMyOwnAccess(t *testing.T) {
@@ -251,11 +225,7 @@ func Test_Invite_RemoveMyOwnAccess(t *testing.T) {
 
 	user, org := getOrg(t)
 
-	w := httptest.NewRecorder()
-	r := requestAs(t, user, "DELETE", "/api/users/org/"+org.ExternalID+"/users/"+user.Email, nil)
-
-	app.ServeHTTP(w, r)
-	assert.Equal(t, http.StatusNoContent, w.Code)
+	requestOrgAs(t, user, "DELETE", org.ExternalID, user.Email, nil, http.StatusNoContent)
 
 	user, err := storage.FindUserByID(user.ID)
 	require.NoError(t, err)
@@ -269,11 +239,7 @@ func Test_Invite_RemoveAccess_Forbidden(t *testing.T) {
 	user, _ := getOrg(t)
 	otherUser, otherOrg := getOrg(t)
 
-	w := httptest.NewRecorder()
-	r := requestAs(t, user, "DELETE", "/api/users/org/"+otherOrg.ExternalID+"/users/"+otherUser.Email, nil)
-
-	app.ServeHTTP(w, r)
-	assert.Equal(t, http.StatusForbidden, w.Code)
+	requestOrgAs(t, user, "DELETE", otherOrg.ExternalID, otherUser.Email, nil, http.StatusForbidden)
 
 	otherUser, err := storage.FindUserByID(otherUser.ID)
 	require.NoError(t, err)
@@ -286,9 +252,5 @@ func Test_Invite_RemoveAccess_NotFound(t *testing.T) {
 
 	user, _ := getOrg(t)
 
-	w := httptest.NewRecorder()
-	r := requestAs(t, user, "DELETE", "/api/users/org/foobar/users/"+user.Email, nil)
-
-	app.ServeHTTP(w, r)
-	assert.Equal(t, http.StatusNotFound, w.Code)
+	requestOrgAs(t, user, "DELETE", "foobar", user.Email, nil, http.StatusNotFound)
 }
