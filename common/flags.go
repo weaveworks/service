d9143c66dbec7050ca2e01a37c2cef3ae26074c3
Tom Wilkie <tom.wilkie@gmail.com>
2016-08-16 17:21:42 +0100

    Spelling

--- common/flags.go
+++ common/flags.go
@@ -4,12 +4,14 @@ import (
 	"strings"
 )
 
+// ArrayFlags allows you to collect repeated flags
 type ArrayFlags []string
 
 func (a *ArrayFlags) String() string {
 	return strings.Join(*a, ",")
 }
 
+// Set implements flags.Value
 func (a *ArrayFlags) Set(value string) error {
 	*a = append(*a, value)
 	return nil
