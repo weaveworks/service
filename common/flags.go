6697b887e1c8681dcfeec0d34599642edb35ec90
Tom Wilkie <tom.wilkie@gmail.com>
2016-08-16 17:02:53 +0100

    Add -fff flag to force a feature flag on for all orgs

---
+++ common/flags.go
@@ -0,0 +1,16 @@
+package common
+
+import (
+	"strings"
+)
+
+type ArrayFlags []string
+
+func (a *ArrayFlags) String() string {
+	return strings.Join(*a, ",")
+}
+
+func (a *ArrayFlags) Set(value string) error {
+	*a = append(*a, value)
+	return nil
+}
