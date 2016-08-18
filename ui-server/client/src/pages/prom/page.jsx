77d6e43b3fba337b56a51f23e2cdb5a41125f21f
David Kaltschmidt <david.kaltschmidt@gmail.com>
2016-08-18 13:29:11 +0100

    Removed unneeded iframe management code

--- ui-server/client/src/pages/prom/page.jsx
+++ ui-server/client/src/pages/prom/page.jsx
@@ -1,68 +1,18 @@
 /* eslint react/jsx-no-bind:0 */
 import React from 'react';
-import ReactDOM from 'react-dom';
-import debug from 'debug';
 
 import { encodeURIs } from '../../common/request';
 import PrivatePage from '../../components/private-page';
 import { trackView } from '../../common/tracking';
 
-const log = debug('service:prom');
-
-export default class Wrapper extends React.Component {
-
-  constructor() {
-    super();
-
-    this.state = {
-      activityText: '',
-      frameBaseUrl: ''
-    };
-
-    this._handleFrameLoad = this._handleFrameLoad.bind(this);
-  }
+export default class PromWrapperPage extends React.Component {
 
   componentDidMount() {
     trackView('Prom');
   }
 
-  componentDidUpdate() {
-    const iframe = ReactDOM.findDOMNode(this._iframe);
-    if (iframe) {
-      // periodically check iframe's URL and react to changes
-      clearInterval(this.frameStateChecker);
-      const target = iframe.contentWindow;
-
-      this.frameStateChecker = setInterval(() => {
-        if (this.frameState !== target.location.hash) {
-          this.frameState = target.location.hash;
-          this._onFrameStateChanged(this.frameState);
-        }
-      }, 1000);
-    }
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.frameStateChecker);
-  }
-
-  _handleFrameLoad(err) {
-    log(err);
-  }
-
-  _onFrameStateChanged() {
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    return this.state.frameBaseUrl !== nextState.frameBaseUrl;
-  }
-
   render() {
     const styles = {
-      activity: {
-        marginTop: 200,
-        textAlign: 'center'
-      },
       iframe: {
         display: 'block',
         border: 'none',
@@ -75,9 +25,8 @@ export default class Wrapper extends React.Component {
     const frameUrl = encodeURIs`/api/app/${orgId}/api/prom/graph`;
 
     return (
-      <PrivatePage page="wrapper" {...this.props.params}>
-        <iframe ref={(c) => {this._iframe = c;}}
-          onLoad={this._handleFrameLoad} src={frameUrl} style={styles.iframe} />}
+      <PrivatePage page="prom" {...this.props.params}>
+        <iframe src={frameUrl} style={styles.iframe} />
       </PrivatePage>
     );
   }
