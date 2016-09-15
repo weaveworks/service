12dd1ca8d28967eeba284bb4ef280589fd2a2c90
Tom Wilkie <tom.wilkie@gmail.com>
2016-09-15 12:42:36 +0100

    Uncomment some random code

--- authfe/proxy.go
+++ authfe/proxy.go
@@ -23,12 +23,10 @@ var proxyTransport http.RoundTripper = &http.Transport{
 
 	// Rest are from http.DefaultTransport
 	Proxy: http.ProxyFromEnvironment,
-	/* Go 1.7 added field
 	DialContext: (&net.Dialer{
 		Timeout:   30 * time.Second,
 		KeepAlive: 30 * time.Second,
 	}).DialContext,
-	*/
 	TLSHandshakeTimeout:   10 * time.Second,
 	ExpectContinueTimeout: 1 * time.Second,
 }
