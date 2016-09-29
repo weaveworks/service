3af91596d81a5bbd0923c31099e9518c965c406e
Jonathan Lange <jml@mumak.net>
2016-09-29 10:27:16 +0100

    Documentation for cpu_by_mem_service.rb

--- monitoring/grafana/generate_cpu_mem_by_service.rb
+++ monitoring/grafana/generate_cpu_mem_by_service.rb
@@ -1,5 +1,11 @@
 #!/usr/bin/env ruby
 # vim:set ft=ruby:
+#
+# Requires Ruby 2.2+
+#
+# Generate `monitoring/grafana/cpu_mem_by_service.json` with:
+#   $ cd monitoring/grafana  # only to make following command shorter
+#   $ ./generate_cpu_mem_by_service.rb | jq -S . > cpu_mem_by_service.json
 
 require 'json'
 
