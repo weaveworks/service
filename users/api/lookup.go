4bad19f411314e1a8704033fd3b1d40179aa2e88
Tom Wilkie <tom.wilkie@gmail.com>
2017-02-24 13:56:05 +0000

    Optimise lookupOrg such that it only does one DB query

--- users/api/lookup.go
+++ users/api/lookup.go
@@ -5,36 +5,61 @@ import (
 	"strings"
 
 	"github.com/gorilla/mux"
+	"golang.org/x/net/context"
 
 	"github.com/weaveworks/service/users"
 	"github.com/weaveworks/service/users/render"
+	"github.com/weaveworks/service/users/sessions"
 )
 
-type lookupOrgView struct {
+type lookupOrgRequest struct {
+	Cookie, OrgExternalID string
+}
+
+type lookupOrgResponse struct {
 	OrganizationID string   `json:"organizationID,omitempty"`
 	UserID         string   `json:"userID,omitempty"`
 	FeatureFlags   []string `json:"featureFlags,omitempty"`
 }
 
-func (a *API) lookupOrg(currentUser *users.User, w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	orgExternalID := vars["orgExternalID"]
-	organizations, err := a.db.ListOrganizationsForUserIDs(r.Context(), currentUser.ID)
+func (a *API) lookupOrgHandler(w http.ResponseWriter, r *http.Request) {
+	cookie, err := sessions.Extract(r)
 	if err != nil {
 		render.Error(w, r, err)
 		return
 	}
+
+	vars := mux.Vars(r)
+	orgExternalID := vars["orgExternalID"]
+	view, err := a.lookupOrg(r.Context(), &lookupOrgRequest{cookie, orgExternalID})
+	if err != nil {
+		render.Error(w, r, err)
+		return
+	}
+
+	render.JSON(w, http.StatusOK, view)
+}
+
+func (a *API) lookupOrg(ctx context.Context, req *lookupOrgRequest) (*lookupOrgResponse, error) {
+	session, err := a.sessions.Decode(req.Cookie)
+	if err != nil {
+		return nil, err
+	}
+
+	organizations, err := a.db.ListOrganizationsForUserIDs(ctx, session.UserID)
+	if err != nil {
+		return nil, err
+	}
 	for _, org := range organizations {
-		if strings.ToLower(org.ExternalID) == strings.ToLower(orgExternalID) {
-			render.JSON(w, http.StatusOK, lookupOrgView{
+		if strings.ToLower(org.ExternalID) == strings.ToLower(req.OrgExternalID) {
+			return &lookupOrgResponse{
 				OrganizationID: org.ID,
-				UserID:         currentUser.ID,
+				UserID:         session.UserID,
 				FeatureFlags:   org.FeatureFlags,
-			})
-			return
+			}, nil
 		}
 	}
-	render.Error(w, r, users.ErrNotFound)
+	return nil, users.ErrInvalidAuthenticationData
 }
 
 type lookupAdminView struct {
@@ -50,7 +75,7 @@ func (a *API) lookupAdmin(currentUser *users.User, w http.ResponseWriter, r *htt
 }
 
 func (a *API) lookupUsingToken(organization *users.Organization, w http.ResponseWriter, r *http.Request) {
-	render.JSON(w, http.StatusOK, lookupOrgView{OrganizationID: organization.ID, FeatureFlags: organization.FeatureFlags})
+	render.JSON(w, http.StatusOK, lookupOrgResponse{OrganizationID: organization.ID, FeatureFlags: organization.FeatureFlags})
 }
 
 type lookupUserView struct {
