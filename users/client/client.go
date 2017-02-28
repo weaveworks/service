e31927739cf77cc3429c5a6884f60d35904209e3
Tom Wilkie <tom.wilkie@gmail.com>
2017-02-28 18:47:33 +0000

    Intercept gRPC errors in the client and tranlate them into something the middleware understands.

--- users/client/client.go
+++ users/client/client.go
@@ -5,8 +5,10 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/grpc-ecosystem/grpc-opentracing/go/otgrpc"
+	"github.com/mwitkow/go-grpc-middleware"
 	"github.com/opentracing/opentracing-go"
 	"github.com/sercand/kuberesolver"
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 
 	"github.com/weaveworks/common/httpgrpc"
@@ -47,12 +49,21 @@ func newGRPCClient(address string) (users.UsersClient, error) {
 		fmt.Sprintf("kubernetes://%s:%s", service, port),
 		balancer.DialOption(),
 		grpc.WithInsecure(),
-		grpc.WithUnaryInterceptor(
+		grpc.WithUnaryInterceptor(grpc_middleware.ChainUnaryClient(
 			otgrpc.OpenTracingClientInterceptor(opentracing.GlobalTracer()),
-		),
+			errorInterceptor,
+		)),
 	)
 	if err != nil {
 		return nil, err
 	}
 	return users.NewUsersClient(conn), nil
+}
+
+var errorInterceptor grpc.UnaryClientInterceptor = func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	err := invoker(ctx, method, req, reply, cc, opts...)
+	if err != nil {
+		return &Unauthorized{int(grpc.Code(err))}
+	}
+	return nil
 }
