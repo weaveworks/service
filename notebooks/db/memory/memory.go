0b1c524ac5be729fb8ee8f93752f99d83ba91232
Aaron Kirkbride <aaron@aaronkirkbride.com>
2017-05-05 10:31:44 +0100

    Update memory db to key on id

--- notebooks/db/memory/memory.go
+++ notebooks/db/memory/memory.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"time"
 
+	uuid "github.com/satori/go.uuid"
 	"github.com/weaveworks/service/notebooks"
 )
 
@@ -11,92 +12,81 @@ var currentID = 1
 
 // DB is an in-memory database for testing, and local development
 type DB struct {
-	notebooks map[string][]notebooks.Notebook
+	notebooks map[string]notebooks.Notebook
 	id        uint
 }
 
 // New creates a new in-memory database
 func New(_, _ string) (*DB, error) {
 	return &DB{
-		notebooks: map[string][]notebooks.Notebook{},
+		notebooks: map[string]notebooks.Notebook{},
 		id:        0,
 	}, nil
 }
 
 // ListNotebooks returns all notebooks for the instance
 func (d DB) ListNotebooks(orgID string) ([]notebooks.Notebook, error) {
-	ns, ok := d.notebooks[orgID]
-	if !ok {
-		return []notebooks.Notebook{}, nil
+	ns := []notebooks.Notebook{}
+	for _, notebook := range d.notebooks {
+		if notebook.OrgID == orgID {
+			ns = append(ns, notebook)
+		}
 	}
 	return ns, nil
 }
 
 // CreateNotebook creates a notebook
 func (d DB) CreateNotebook(notebook notebooks.Notebook) error {
-	ns, ok := d.notebooks[notebook.OrgID]
-	if !ok {
-		ns = []notebooks.Notebook{}
-	}
-	ns = append(ns, notebook)
-	d.notebooks[notebook.OrgID] = ns
+	notebook.ID = uuid.NewV4()
+	notebook.CreatedAt = time.Now()
+	notebook.UpdatedAt = time.Now()
+
+	d.notebooks[notebook.ID.String()] = notebook
 	return nil
 }
 
 // GetNotebook returns all notebooks for the instance
 func (d DB) GetNotebook(ID, orgID string) (notebooks.Notebook, error) {
-	ns, ok := d.notebooks[orgID]
-	if !ok {
-		return notebooks.Notebook{}, errors.New("Org not found")
-	}
-
-	for _, notebook := range ns {
-		if notebook.ID.String() == ID {
-			return notebook, nil
+	if notebook, ok := d.notebooks[ID]; ok {
+		if notebook.OrgID != orgID {
+			return notebooks.Notebook{}, errors.New("Notebook not found")
 		}
+		return notebook, nil
 	}
 	return notebooks.Notebook{}, errors.New("Notebook not found")
 }
 
 // UpdateNotebook updates a notebook
 func (d DB) UpdateNotebook(ID, orgID string, update notebooks.Notebook) error {
-	ns, ok := d.notebooks[orgID]
-	if !ok {
-		return errors.New("Org not found")
-	}
-
-	var updatedNotebooks []notebooks.Notebook
-	for _, notebook := range ns {
-		if notebook.ID.String() == ID {
-			notebook.UpdatedBy = update.UpdatedBy
-			notebook.UpdatedAt = time.Now()
-			notebook.Title = update.Title
-			notebook.Entries = update.Entries
+	if notebook, ok := d.notebooks[ID]; ok {
+		if notebook.OrgID != orgID {
+			return errors.New("Notebook not found")
 		}
-		updatedNotebooks = append(updatedNotebooks, notebook)
+
+		notebook.UpdatedBy = update.UpdatedBy
+		notebook.UpdatedAt = time.Now()
+		notebook.Title = update.Title
+		notebook.Entries = update.Entries
+
+		d.notebooks[orgID] = notebook
+		return nil
 	}
-	d.notebooks[orgID] = updatedNotebooks
-	return nil
+	return errors.New("Notebook not found")
 }
 
 // DeleteNotebook deletes a notebook
 func (d DB) DeleteNotebook(ID, orgID string) error {
-	ns, ok := d.notebooks[orgID]
-	if !ok {
-		return errors.New("Org not found")
-	}
-
-	var updatedNotebooks []notebooks.Notebook
-	for _, notebook := range ns {
-		if notebook.ID.String() != ID {
-			updatedNotebooks = append(updatedNotebooks, notebook)
+	if notebook, ok := d.notebooks[ID]; ok {
+		if notebook.OrgID != orgID {
+			return errors.New("Notebook not found")
 		}
+		delete(d.notebooks, ID)
+		return nil
 	}
-	d.notebooks[orgID] = updatedNotebooks
-	return nil
+	return errors.New("Notebook not found")
 }
 
-// Close finishes using the db. Noop.
+// Close the database. Noop.
 func (d *DB) Close() error {
 	return nil
 }
