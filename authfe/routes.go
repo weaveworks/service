91527502427a3bc2c1f4cc7c93672bc709c9e33c
Tom Wilkie <tomwilkie@users.noreply.github.com>
2017-05-08 12:24:15 +0100

    Add 'unsafe-inline' to CSP (#1204)

--- authfe/routes.go
+++ authfe/routes.go
@@ -37,7 +37,7 @@ func (c Config) commonMiddleWare(routeMatcher middleware.RouteMatcher) middlewar
 	extraHeaders.Add("X-XSS-Protection", "1; mode=block")
 	extraHeaders.Add("X-Content-Type-Options", "nosniff")
 	if c.sendCSPHeader {
-		extraHeaders.Add("Content-Security-Policy", "default-src https:")
+		extraHeaders.Add("Content-Security-Policy", "default-src https: 'unsafe-inline'")
 	}
 	if c.redirectHTTPS && c.hstsMaxAge > 0 {
 		extraHeaders.Add("Strict-Transport-Security", fmt.Sprintf("max-age=%d; includeSubDomains", c.hstsMaxAge))
