72835efbc89322812676d3459a83ff9c0d3f7038
Aaron Kirkbride <aaron@aaronkirkbride.com>
2017-05-15 10:36:14 +0100

    Allow entries to be null

--- notebooks/db/migrations/003_set_not_nulls.up.sql
+++ notebooks/db/migrations/003_set_not_nulls.up.sql
@@ -4,5 +4,4 @@ ALTER TABLE notebooks
     ALTER COLUMN created_at SET NOT NULL,
     ALTER COLUMN updated_by SET NOT NULL,
     ALTER COLUMN updated_at SET NOT NULL,
-    ALTER COLUMN title SET NOT NULL,
-    ALTER COLUMN entries SET NOT NULL;
+    ALTER COLUMN title SET NOT NULL;
