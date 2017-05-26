45feb350bd4490975d51cbcaf014e8f71fcbe16d
Tom Wilkie <tom.wilkie@gmail.com>
2017-05-26 13:26:17 +0100

    Vendor with dep; install protoc from debian package.

---
+++ vendor/github.com/bluele/gcache/cache_test.go
@@ -0,0 +1,113 @@
+package gcache_test
+
+import (
+	"bytes"
+	"encoding/gob"
+	"testing"
+	"time"
+
+	"sync"
+	"sync/atomic"
+
+	"github.com/bluele/gcache"
+)
+
+func TestLoaderFunc(t *testing.T) {
+	size := 2
+	var testCaches = []*gcache.CacheBuilder{
+		gcache.New(size).Simple(),
+		gcache.New(size).LRU(),
+		gcache.New(size).LFU(),
+		gcache.New(size).ARC(),
+	}
+	for _, builder := range testCaches {
+		var testCounter int64
+		counter := 1000
+		cache := builder.
+			LoaderFunc(func(key interface{}) (interface{}, error) {
+				time.Sleep(10 * time.Millisecond)
+				return atomic.AddInt64(&testCounter, 1), nil
+			}).
+			EvictedFunc(func(key, value interface{}) {
+				panic(key)
+			}).Build()
+
+		var wg sync.WaitGroup
+		for i := 0; i < counter; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				_, err := cache.Get(0)
+				if err != nil {
+					t.Error(err)
+				}
+			}()
+		}
+		wg.Wait()
+
+		if testCounter != 1 {
+			t.Errorf("testCounter != %v", testCounter)
+		}
+	}
+}
+
+func TestDeserializeFunc(t *testing.T) {
+	var cases = []struct {
+		tp string
+	}{
+		{gcache.TYPE_SIMPLE},
+		{gcache.TYPE_LRU},
+		{gcache.TYPE_LFU},
+		{gcache.TYPE_ARC},
+	}
+
+	for _, cs := range cases {
+		key1, value1 := "key1", "value1"
+		key2, value2 := "key2", "value2"
+		cc := gcache.New(32).
+			EvictType(cs.tp).
+			LoaderFunc(func(k interface{}) (interface{}, error) {
+				return value1, nil
+			}).
+			DeserializeFunc(func(k, v interface{}) (interface{}, error) {
+				dec := gob.NewDecoder(bytes.NewBuffer(v.([]byte)))
+				var str string
+				err := dec.Decode(&str)
+				if err != nil {
+					return nil, err
+				}
+				return str, nil
+			}).
+			SerializeFunc(func(k, v interface{}) (interface{}, error) {
+				buf := new(bytes.Buffer)
+				enc := gob.NewEncoder(buf)
+				err := enc.Encode(v)
+				return buf.Bytes(), err
+			}).
+			Build()
+		v, err := cc.Get(key1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if v != value1 {
+			t.Errorf("%v != %v", v, value1)
+		}
+		v, err = cc.Get(key1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if v != value1 {
+			t.Errorf("%v != %v", v, value1)
+		}
+		if err := cc.Set(key2, value2); err != nil {
+			t.Error(err)
+		}
+		v, err = cc.Get(key2)
+		if err != nil {
+			t.Error(err)
+		}
+		if v != value2 {
+			t.Errorf("%v != %v", v, value2)
+		}
+	}
+}
---
+++ vendor/github.com/weaveworks/common/httpgrpc/httpgrpc.go
@@ -0,0 +1,216 @@
+package httpgrpc
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"sync"
+
+	"github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/any"
+	"github.com/grpc-ecosystem/grpc-opentracing/go/otgrpc"
+	"github.com/mwitkow/go-grpc-middleware"
+	"github.com/opentracing/opentracing-go"
+	"github.com/prometheus/common/log"
+	"github.com/sercand/kuberesolver"
+	"golang.org/x/net/context"
+	spb "google.golang.org/genproto/googleapis/rpc/status"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
+
+	"github.com/weaveworks/common/middleware"
+)
+
+// Server implements HTTPServer.  HTTPServer is a generated interface that gRPC
+// servers must implement.
+type Server struct {
+	handler http.Handler
+}
+
+// NewServer makes a new Server.
+func NewServer(handler http.Handler) *Server {
+	return &Server{
+		handler: handler,
+	}
+}
+
+// Handle implements HTTPServer.
+func (s Server) Handle(ctx context.Context, r *HTTPRequest) (*HTTPResponse, error) {
+	req, err := http.NewRequest(r.Method, r.Url, ioutil.NopCloser(bytes.NewReader(r.Body)))
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(ctx)
+	toHeader(r.Headers, req.Header)
+	req.RequestURI = r.Url
+	recorder := httptest.NewRecorder()
+	s.handler.ServeHTTP(recorder, req)
+	resp := &HTTPResponse{
+		Code:    int32(recorder.Code),
+		Headers: fromHeader(recorder.Header()),
+		Body:    recorder.Body.Bytes(),
+	}
+	if recorder.Code/100 == 5 {
+		return nil, errorFromHTTPResponse(resp)
+	}
+	return resp, err
+}
+
+// Client is a http.Handler that forwards the request over gRPC.
+type Client struct {
+	mtx       sync.RWMutex
+	service   string
+	namespace string
+	port      string
+	client    HTTPClient
+	conn      *grpc.ClientConn
+}
+
+// ParseURL deals with direct:// style URLs, as well as kubernetes:// urls.
+// For backwards compatibility it treats URLs without schems as kubernetes://.
+func ParseURL(unparsed string) (string, []grpc.DialOption, error) {
+	parsed, err := url.Parse(unparsed)
+	if err != nil {
+		return "", nil, err
+	}
+
+	switch parsed.Scheme {
+	case "direct":
+		return parsed.Host, nil, err
+
+	case "kubernetes", "":
+		host, port, err := net.SplitHostPort(parsed.Host)
+		if err != nil {
+			return "", nil, err
+		}
+		parts := strings.SplitN(host, ".", 2)
+		service, namespace := parts[0], "default"
+		if len(parts) == 2 {
+			namespace = parts[1]
+		}
+		balancer := kuberesolver.NewWithNamespace(namespace)
+		address := fmt.Sprintf("kubernetes://%s:%s", service, port)
+		dialOptions := []grpc.DialOption{balancer.DialOption()}
+		return address, dialOptions, nil
+
+	default:
+		return "", nil, fmt.Errorf("unrecognised scheme: %s", parsed.Scheme)
+	}
+}
+
+// NewClient makes a new Client, given a kubernetes service address.
+func NewClient(address string) (*Client, error) {
+	address, dialOptions, err := ParseURL(address)
+	if err != nil {
+		return nil, err
+	}
+
+	dialOptions = append(
+		dialOptions,
+		grpc.WithInsecure(),
+		grpc.WithUnaryInterceptor(grpc_middleware.ChainUnaryClient(
+			otgrpc.OpenTracingClientInterceptor(opentracing.GlobalTracer()),
+			middleware.ClientUserHeaderInterceptor,
+		)),
+	)
+
+	conn, err := grpc.Dial(address, dialOptions...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		client: NewHTTPClient(conn),
+		conn:   conn,
+	}, nil
+}
+
+// ServeHTTP implements http.Handler
+func (c *Client) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	req := &HTTPRequest{
+		Method:  r.Method,
+		Url:     r.RequestURI,
+		Body:    body,
+		Headers: fromHeader(r.Header),
+	}
+
+	resp, err := c.client.Handle(r.Context(), req)
+	if err != nil {
+		// Some errors will actually contain a valid resp, just need to unpack it
+		var ok bool
+		resp, ok = httpResponseFromError(err)
+
+		if !ok {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	toHeader(resp.Headers, w.Header())
+	w.WriteHeader(int(resp.Code))
+	if _, err := w.Write(resp.Body); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func errorFromHTTPResponse(resp *HTTPResponse) error {
+	a, err := ptypes.MarshalAny(resp)
+	if err != nil {
+		return err
+	}
+
+	return status.ErrorProto(&spb.Status{
+		Code:    resp.Code,
+		Message: string(resp.Body),
+		Details: []*any.Any{a},
+	})
+}
+
+func httpResponseFromError(err error) (*HTTPResponse, bool) {
+	s, ok := status.FromError(err)
+	if !ok {
+		fmt.Println("not status")
+		return nil, false
+	}
+
+	status := s.Proto()
+	if len(status.Details) != 1 {
+		return nil, false
+	}
+
+	var resp HTTPResponse
+	if err := ptypes.UnmarshalAny(status.Details[0], &resp); err != nil {
+		log.Errorf("Got error containing non-response: %v", err)
+		return nil, false
+	}
+
+	return &resp, true
+}
+
+func toHeader(hs []*Header, header http.Header) {
+	for _, h := range hs {
+		header[h.Key] = h.Values
+	}
+}
+
+func fromHeader(hs http.Header) []*Header {
+	result := make([]*Header, 0, len(hs))
+	for k, vs := range hs {
+		result = append(result, &Header{
+			Key:    k,
+			Values: vs,
+		})
+	}
+	return result
+}
