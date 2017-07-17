a992e4d1fcd03c40f031b41d221f1a43302c1d72
meccanorak <robin@weave.works>
2017-07-17 11:46:15 +0100

    Added comment cross-referencing to where UI enforces same constraint

--- users/db/migrations/015_limit_length_organizations.up.sql
+++ users/db/migrations/015_limit_length_organizations.up.sql
@@ -1,3 +1,6 @@
 -- When we change DBcolumn type from text to varchar(100)
 -- any fields with existing data beyond new limit are simply truncated
+
+-- Constraint here needs to match UI-enforced length constraint see
+-- service-ui:client/src/common/constants.js:INSTANCE_NAME_MAX_LENGTH
 ALTER TABLE organizations ALTER name TYPE varchar(100) USING left(name,100);
