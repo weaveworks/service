fd593c52aa1e9cfd6221155af0d8ae0b848da561
Jonathan Lange <jml@mumak.net>
2017-08-22 17:16:40 +0100

    Allow filtering to show admins only

--- users/db/db.go
+++ users/db/db.go
@@ -48,7 +48,7 @@ type DB interface {
 	// Remove a user from an organization. If they do not exist (ctx context.Context, or are not a member of the org), return success.
 	RemoveUserFromOrganization(ctx context.Context, orgExternalID, email string) error
 
-	ListUsers(ctx context.Context) ([]*users.User, error)
+	ListUsers(ctx context.Context, adminOnly bool) ([]*users.User, error)
 	ListOrganizations(ctx context.Context) ([]*users.Organization, error)
 	ListOrganizationUsers(ctx context.Context, orgExternalID string) ([]*users.User, error)
 
