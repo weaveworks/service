ab164ac87bcfacd6d120d62413a1e94de77548e6
Aaron Kirkbride <aaron@aaronkirkbride.com>
2017-09-06 09:23:39 +0100

    Backfill organizations first_seen_connected_at

---
+++ users/db/backfills/20170805_backfill_org_first_seen_connected_at.py
@@ -0,0 +1,48 @@
+from google.cloud import bigquery
+import psycopg2
+
+# Users database
+DB_HOST = "prod-users-vpc-database.cqqzyyx2xnct.us-east-1.rds.amazonaws.com"
+DB_NAME = "users"
+DB_USER = "postgres"
+DB_PASS = "" # https://github.com/weaveworks/service-conf/blob/master/infra/prod/tfstate
+
+# BigQuery query. Requires Google Cloud application default credentials setup locally.
+FIRST_CONNECTED_QUERY = """
+SELECT org_id, MIN(dt) as first
+    FROM service.events
+    WHERE event = "/api/report" OR event = "/api/prom/push" OR event LIKE "/api/flux/v_/daemon"
+    GROUP BY org_id
+"""
+
+def run_backfill():
+    client = bigquery.Client(project='weaveworks-bi')
+    query = client.run_sync_query(FIRST_CONNECTED_QUERY)
+    query.timeout_ms = 120 * 1000
+
+    print("Running BigQuery query...")
+    query.run()
+    assert query.complete
+    print("BigQuery query complete.")
+
+    print("Connecting to postgres database...")
+    conn_string = "host={} dbname={} user={} password={}".format(DB_HOST, DB_NAME, DB_USER, DB_PASS)
+    conn = psycopg2.connect(conn_string)
+    cur = conn.cursor()
+
+    print("Running backfill...")
+    for org_id, first in query.fetch_data():
+        if first:
+            print("Setting orgID '{}' first_seen_connected_at to '{}'".format(org_id, first))
+            sql = "UPDATE organizations SET first_seen_connected_at = '%s' WHERE id = '%s'"
+            cur.execute(sql, (first, org_id))
+
+    print("Committing...")
+    conn.commit()
+    print("Closing database connection...")
+    cur.close()
+    conn.close()
+    print("Backfill complete.")
+
+if __name__ == "__main__":
+    run_backfill()
