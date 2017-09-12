e45440f8cf249f99cbb104a5ac6284ec016a95d3
Bryan Boreham <bjboreham@gmail.com>
2017-09-12 14:10:15 +0000

    Removed unused vendor files using 'dep prune'

--- vendor/github.com/weaveworks/scope/client/app/scripts/selectors/node-filters.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from 'reselect';
-
-
-export const shownNodesSelector = createSelector(
-  [
-    state => state.get('nodes'),
-  ],
-  nodes => nodes.filter(node => !node.get('filtered'))
-);
