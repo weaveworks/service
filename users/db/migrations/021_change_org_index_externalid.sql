e5a592ad5b48c78a01a34573e51d01d559011383
Roland Schilter <roli@weave.works>
2017-09-13 10:29:21 +0100

    Add missing index name suffix `_idx`

--- users/db/migrations/021_change_org_index_externalid.sql
+++ users/db/migrations/021_change_org_index_externalid.sql
@@ -1,2 +1,2 @@
 DROP INDEX organizations_lower_external_id_idx;
-CREATE UNIQUE INDEX organizations_external_id ON organizations USING btree(external_id);
+CREATE UNIQUE INDEX organizations_external_id_idx ON organizations USING btree(external_id);
