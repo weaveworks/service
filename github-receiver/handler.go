a23818679021943de9826592f0cec300d8f14089
Sam Broughton <sam@weave.works>
2017-09-21 16:31:38 +0300

    Use logrus and common/server

--- github-receiver/handler.go
+++ github-receiver/handler.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"log"
 	"net/http"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/google/go-github/github"
 )
 
--- github-receiver/main.go
+++ github-receiver/main.go
@@ -2,21 +2,35 @@ package main
 
 import (
 	"flag"
-	"log"
-	"net/http"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/weaveworks/common/server"
 )
 
 func main() {
 	var (
 		fluxSvcURL    = flag.String("flux-svc-url", "fluxsvc.flux.svc.cluster.local.:80", "Flux service base URL")
 		webhookSecret = flag.String("webhook-secret", "", "Github App webhook secret")
+		cfg           = server.Config{
+			MetricsNamespace: "github-receiver",
+		}
 	)
+	cfg.RegisterFlags(flag.CommandLine)
 	flag.Parse()
 
 	if *webhookSecret == "" {
 		log.Fatal("webhook secret not set")
 	}
 
-	http.Handle("/webhook", makeHandler(*fluxSvcURL, []byte(*webhookSecret)))
-	http.ListenAndServe(":80", nil)
+	server, err := server.New(cfg)
+	if err != nil {
+		log.Fatal("error initialising server:", err)
+	}
+	defer server.Shutdown()
+
+	server.HTTP.Handle(
+		"/webhook",
+		makeHandler(*fluxSvcURL, []byte(*webhookSecret)),
+	).Methods("POST").Name("receive_webhook")
+	server.Run()
 }
