71871a3137fed082b8298698ebb7bf17123a964b
Marcus Cobden <marcus@weave.works>
2017-09-22 14:39:48 -0700

    Update tests to match escaping and interface

--- users/api/signup_test.go
+++ users/api/signup_test.go
@@ -20,7 +20,7 @@ import (
 )
 
 func findLoginLink(t *testing.T, e *email.Email) (url, token string) {
-	pattern := domain + `/login/[\w.%]+/([A-Za-z0-9%._=-]+)`
+	pattern := domain + `/login/[\w.@]+/([A-Za-z0-9%._=-]+)`
 	re := regexp.MustCompile(pattern)
 	matches := re.FindStringSubmatch(string(e.Text))
 	require.Len(t, matches, 2, fmt.Sprintf("Could not find Login Link in text: %q", e.Text))
