9086a5e5b34c984b853901fbc9096c7b66170241
Roland Schilter <roli@weave.works>
2017-11-02 13:32:12 +0000

    Add tests for webhook

---
+++ common/gcp/pubsub/webhook/webhook_test.go
@@ -0,0 +1,46 @@
+package webhook_test
+
+import (
+	"bytes"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/weaveworks/service/common/gcp/pubsub/dto"
+	"github.com/weaveworks/service/common/gcp/pubsub/webhook"
+)
+
+type handler struct {
+	err error
+}
+
+func (h handler) Handle(m dto.Message) error {
+	return h.err
+}
+
+func TestWebhook_success(t *testing.T) {
+	rec := doRequest(t, &handler{}, `{}`)
+	assert.Equal(t, http.StatusNoContent, rec.Code)
+}
+
+func TestWebhook_bodyFail(t *testing.T) {
+	rec := doRequest(t, &handler{}, `{`)
+	assert.Equal(t, http.StatusBadRequest, rec.Code)
+}
+
+func TestWebhook_fail(t *testing.T) {
+	rec := doRequest(t, &handler{err: errors.New("boom")}, `{}`)
+	assert.Equal(t, http.StatusInternalServerError, rec.Code)
+}
+
+func doRequest(t *testing.T, h webhook.MessageHandler, js string) *httptest.ResponseRecorder {
+	r, err := http.NewRequest("POST", "", bytes.NewBufferString(js))
+	assert.NoError(t, err)
+	w := httptest.NewRecorder()
+
+	webhook.New(h).ServeHTTP(w, r)
+	return w
+}
