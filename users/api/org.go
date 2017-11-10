ac2c00d29d4bacddc0b3e80151549bc8cb485e66
Roland Schilter <roli@weave.works>
2017-11-10 12:38:34 +0000

    API: pass `isGCP` to frontend

--- users/api/org.go
+++ users/api/org.go
@@ -33,6 +33,7 @@ type OrgView struct {
 	TrialExpiresAt        time.Time  `json:"trialExpiresAt"`
 	ZuoraAccountNumber    string     `json:"zuoraAccountNumber"`
 	ZuoraAccountCreatedAt *time.Time `json:"zuoraAccountCreatedAt"`
+	IsGCP                 bool       `json:"isGCP"`
 }
 
 func (a *API) org(currentUser *users.User, w http.ResponseWriter, r *http.Request) {
@@ -57,6 +58,7 @@ func (a *API) org(currentUser *users.User, w http.ResponseWriter, r *http.Reques
 				Platform:             org.Platform,
 				Environment:          org.Environment,
 				TrialExpiresAt:       org.TrialExpiresAt,
+				IsGCP:                org.GCP != nil,
 			})
 			return
 		}
