388636b65d274cbd791070a5a3a50bc9ab9db4e6
Marc CARRE <carre.marc@gmail.com>
2017-11-17 19:33:28 +0000

    Re-create TYPE uploader_type to add 'gcp' to the enum.
    Otherwise PostgreSQL and our DB migrations framework say 'no': 'ERROR 25001: ALTER TYPE ... ADD cannot run inside a transaction block'.

--- billing-api/db/migrations/006_add_uploader_type_gcp.up.sql
+++ billing-api/db/migrations/006_add_uploader_type_gcp.up.sql
@@ -1,1 +1,11 @@
-ALTER TYPE uploader_type ADD VALUE 'gcp';
+-- Add 'gcp' to uploader_type by renaming the type and recreating it with the new value,
+-- because we otherwise get: ERROR 25001: ALTER TYPE ... ADD cannot run inside a transaction block"
+-- with our DB migrations framework and PostgreSQL.
+ALTER TYPE uploader_type RENAME TO uploader_type_old;
+CREATE TYPE uploader_type AS enum ('zuora', 'gcp');
+
+-- Alter all previous values:
+ALTER TABLE usage_uploads ALTER COLUMN uploader TYPE uploader_type USING uploader::text::uploader_type;
+
+-- Drop the old enum:
+DROP TYPE uploader_type_old;
