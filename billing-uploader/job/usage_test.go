e75083807189c33c31e0efa1548692b609a78d1b
Roland Schilter <roli@weave.works>
2017-11-28 14:12:40 +0000

    Add test for GCP usage upload
    And move it to the proper directory.

--- billing-uploader/job/usage_test.go
+++ billing-uploader/job/usage_test.go
@@ -1,4 +1,4 @@
-package main_test
+package job_test
 
 import (
 	"bytes"
@@ -12,6 +12,7 @@ import (
 
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/api/servicecontrol/v1"
 
 	"github.com/weaveworks/common/instrument"
 	"github.com/weaveworks/service/billing-api/db"
@@ -46,7 +47,7 @@ func (z *stubZuoraClient) UploadUsage(ctx context.Context, r io.Reader) (string,
 	return "", z.err
 }
 
-func TestJobUpload_Do(t *testing.T) {
+func TestJobUpload_Do_zuora(t *testing.T) {
 	d := dbtest.Setup(t)
 	defer dbtest.Cleanup(t, d)
 
@@ -135,7 +136,7 @@ func TestJobUpload_Do(t *testing.T) {
 	assert.Equal(t, 4, aggID)
 }
 
-func TestJobUpload_DoError(t *testing.T) {
+func TestJobUpload_Do_zuoraError(t *testing.T) {
 	d := dbtest.Setup(t)
 	defer dbtest.Cleanup(t, d)
 
@@ -173,4 +174,117 @@ func TestJobUpload_DoError(t *testing.T) {
 	assert.NoError(t, err)
 	// Make sure the max_aggregate_id was removed after failing to upload
 	assert.Equal(t, 0, aggID)
+}
+
+// stubControlClient implements control.API
+type stubControlClient struct {
+	operations []*servicecontrol.Operation
+}
+
+func (c *stubControlClient) Report(ctx context.Context, operations []*servicecontrol.Operation) error {
+	c.operations = operations
+	return nil
+}
+
+func (c *stubControlClient) OperationID(name string) string {
+	return "opid"
+}
+
+func TestJobUpload_Do_gcp(t *testing.T) {
+	d := dbtest.Setup(t)
+	defer dbtest.Cleanup(t, d)
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	ctx := context.Background()
+
+	cl := &stubControlClient{}
+	u := mock_users.NewMockUsersClient(ctrl)
+	start := time.Now()
+	u.EXPECT().
+		GetBillableOrganizations(gomock.Any(), gomock.Any()).
+		Return(&users.GetBillableOrganizationsResponse{
+			Organizations: []users.Organization{
+				{
+					ID:         "1",
+					ExternalID: "yes",
+					GCP: &users.GoogleCloudPlatform{
+						ExternalAccountID:  "F-EEE",
+						ConsumerID:         "project_number:123",
+						Activated:          true,
+						SubscriptionName:   "partnerSubscriptions/123",
+						SubscriptionLevel:  "standard",
+						SubscriptionStatus: "ACTIVE",
+					},
+				},
+				{
+					ID:         "10",
+					ExternalID: "skip-inactivated",
+					GCP: &users.GoogleCloudPlatform{
+						ExternalAccountID:  "F-EEE",
+						ConsumerID:         "project_number:123",
+						Activated:          false,
+						SubscriptionName:   "partnerSubscriptions/123",
+						SubscriptionLevel:  "standard",
+						SubscriptionStatus: "ACTIVE",
+					},
+				},
+				{
+					ID:         "11",
+					ExternalID: "skip-inactive_subscription",
+					GCP: &users.GoogleCloudPlatform{
+						ExternalAccountID:  "F-EEE",
+						ConsumerID:         "project_number:123",
+						Activated:          true,
+						SubscriptionStatus: "",
+					},
+				},
+			},
+		}, nil)
+	ns := "node-seconds"
+	aggregates := []db.Aggregate{
+		{
+			BucketStart: start,
+			InstanceID:  "1",
+			AmountType:  ns,
+			AmountValue: 10800,
+		},
+		{
+			BucketStart: start.Add(-1 * time.Hour),
+			InstanceID:  "1",
+			AmountType:  ns,
+			AmountValue: 0,
+		},
+		{
+			BucketStart: start,
+			InstanceID:  "10",
+			AmountType:  ns,
+			AmountValue: 2,
+		},
+	}
+	err := d.UpsertAggregates(ctx, aggregates)
+	assert.NoError(t, err)
+	_, err = d.InsertUsageUpload(ctx, "gcp", 0)
+	assert.NoError(t, err)
+
+	j := job.NewUsageUpload(d, u, usage.NewGCP(cl), instrument.NewJobCollector("foo"))
+	err = j.Do()
+	assert.NoError(t, err)
+
+	assert.Len(t, cl.operations, 2)
+	first := cl.operations[0]
+	second := cl.operations[1]
+	// Proper assignment of usage
+	assert.Equal(t, int64(10800), *first.MetricValueSets[0].MetricValues[0].Int64Value)
+	assert.Equal(t, int64(0), *second.MetricValueSets[0].MetricValues[0].Int64Value)
+
+	assert.Equal(t, "google.weave.works/standard_nodes", first.MetricValueSets[0].MetricName)
+	assert.Equal(t, "project_number:123", first.ConsumerId)
+	assert.Equal(t, "opid", first.OperationId)
+	assert.Equal(t, "HourlyUsageUpload", first.OperationName)
+
+	aggID, err := d.GetUsageUploadLargestAggregateID(ctx, "gcp")
+	assert.NoError(t, err)
+	assert.Equal(t, 2, aggID)
 }
