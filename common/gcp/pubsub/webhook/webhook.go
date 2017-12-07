d3d2636d3d1a41afe2e75f6fc8e6761a101d72d6
Roland Schilter <roli@weave.works>
2017-12-07 17:28:16 +0000

    Add pubsub total messages metric

--- common/gcp/pubsub/webhook/webhook.go
+++ common/gcp/pubsub/webhook/webhook.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/weaveworks/service/common/gcp/pubsub/dto"
@@ -12,22 +13,39 @@ import (
 	users_render "github.com/weaveworks/service/users/render"
 )
 
+var (
+	receivedMessages = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "service",
+		Subsystem: "pubsub",
+		Name:      "received_messages_total",
+		Help:      "Number of received messages by the PubSub webhook.",
+	}, []string{"status"})
+)
+
+func init() {
+	prometheus.MustRegister(receivedMessages)
+}
+
 // New returns a http.Handler configured to be able to handle Google Pub/Sub events.
 // It requires a MessageHandler to be provided to act upon the message.
 func New(handler MessageHandler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		event := dto.Event{}
-		if err := json.NewDecoder(req.Body).Decode(&event); err != nil {
-			// NACK: we might want to retry on this message later.
-			render.Error(w, req, users.NewMalformedInputError(err), users_render.ErrorStatusCode)
-			return
-		}
-		log.Infof("Incoming webhook event: %+v", event)
+		var err error
 
-		if err := handler.Handle(event.Message); err != nil {
+		event := dto.Event{}
+		if err = json.NewDecoder(req.Body).Decode(&event); err != nil {
+			err = users.NewMalformedInputError(err) // NACK: we might want to retry on this message later.
+		} else {
+			log.Infof("Incoming webhook event: %+v", event)
+			err = handler.Handle(event.Message)
+		}
+
+		if err != nil {
 			render.Error(w, req, err, users_render.ErrorStatusCode) // NACK: we might want to retry on this message later.
+			receivedMessages.WithLabelValues("error").Inc()
 		} else {
 			w.WriteHeader(http.StatusNoContent) // ACK: remove this message from Pub/Sub.
+			receivedMessages.WithLabelValues("success").Inc()
 		}
 	})
 }
