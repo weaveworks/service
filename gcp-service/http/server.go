9712b63f6d9fd906d1c689dde00387cd4e9948b8
Aaron Kirkbride <aaron@aaronkirkbride.com>
2018-02-20 15:14:52 +0000

    gcp-service: Create endpoint to install WC via new launcher

--- gcp-service/http/server.go
+++ gcp-service/http/server.go
@@ -1,18 +1,20 @@
 package http
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/weaveworks/launcher/pkg/kubectl"
 
 	"github.com/weaveworks/service/common/render"
 	gcprender "github.com/weaveworks/service/gcp-service/render"
 	"github.com/weaveworks/service/gcp-service/service"
 
 	"github.com/gorilla/mux"
-
-	"fmt"
-	"io/ioutil"
 )
 
 // Server handles HTTP requests/responses, but delegates actual servicing of requests to... Service.
@@ -45,6 +47,9 @@ func (s Server) RegisterRoutes(r *mux.Router) {
 
 		// Run the provided kubectl command (as a JSON array in the request's body) to the specified GKE cluster (belonging to the specified user, and existing in the specified zone):
 		{"users_projects_clusters_zones_kubectl", "POST", fmt.Sprintf("/api/gcp/users/{%v}/projects/{%v}/clusters/{%v}/zones/{%v}/kubectl", UserID, ProjectID, ClusterID, Zone), s.RunKubectlCmd},
+
+		// Install Weave Cloud to the specified GKE cluster (belonging to the specified user, and existing in the specified zone):
+		{"users_projects_clusters_zones_install", "POST", fmt.Sprintf("/api/gcp/users/{%v}/projects/{%v}/clusters/{%v}/zones/{%v}/install", UserID, ProjectID, ClusterID, Zone), s.InstallWeaveCloud},
 	} {
 		r.Handle(route.path, route.handler).Methods(route.method).Name(route.name)
 	}
@@ -113,4 +118,64 @@ func deserializeStringArray(body io.ReadCloser) ([]string, error) {
 		return nil, err
 	}
 	return strings, nil
+}
+
+// KubectlServiceClient implements kubectl.Client
+type KubectlServiceClient struct {
+	Context   context.Context
+	Service   *service.Service
+	UserID    string
+	ProjectID string
+	Zone      string
+	ClusterID string
+}
+
+// Execute implements kubectl.Client
+func (k KubectlServiceClient) Execute(args ...string) (string, error) {
+	return k.Service.RunKubectlCmd(k.Context, k.UserID, k.ProjectID, k.Zone, k.ClusterID, args)
+}
+
+// InstallWeaveCloud executes the provided kubectl command against the specified cluster.
+func (s Server) InstallWeaveCloud(w http.ResponseWriter, r *http.Request) {
+	userID := mux.Vars(r)[UserID]
+	projectID := mux.Vars(r)[ProjectID]
+	zone := mux.Vars(r)[Zone]
+	clusterID := mux.Vars(r)[ClusterID]
+
+	var payload struct {
+		Token string `json:"token"`
+	}
+	json.NewDecoder(r.Body).Decode(&payload)
+
+	client := KubectlServiceClient{
+		Context:   r.Context(),
+		Service:   s.Service,
+		UserID:    userID,
+		ProjectID: projectID,
+		Zone:      zone,
+		ClusterID: clusterID,
+	}
+
+	// 1. Create weave namespace
+	_, err := kubectl.CreateNamespace(client, "weave")
+	if err != nil {
+		render.Error(w, r, err, gcprender.ErrorStatusCode)
+		return
+	}
+
+	// 2. Create weave-cloud token secret
+	_, err = kubectl.CreateSecretFromLiteral(client, "weave", "weave-cloud", "token", payload.Token, true)
+	if err != nil {
+		render.Error(w, r, err, gcprender.ErrorStatusCode)
+		return
+	}
+
+	// 3. Apply agent k8s
+	err = kubectl.Apply(client, "https://get.weave.works/k8s/agent.yaml")
+	if err != nil {
+		render.Error(w, r, err, gcprender.ErrorStatusCode)
+		return
+	}
+
+	render.JSON(w, http.StatusOK, nil)
 }
