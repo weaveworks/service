7c6993a9ac087a9f9b336be5fb131671990cdb5b
Damien Lespiau <damien@weave.works>
2018-03-16 17:22:31 +0000

    dashboard: Add a basic JVM dashboard

---
+++ dashboard-api/dashboard/java-jvm.go
@@ -0,0 +1,66 @@
+package dashboard
+
+var jvmDashboard = Dashboard{
+	ID:   "java-jvm",
+	Name: "JVM",
+	Sections: []Section{{
+		Name: "Concurrency",
+		Rows: []Row{{
+			Panels: []Panel{{
+				Title: "Threads",
+				Help:  "Current number of live threads including both daemon and non-daemon threads",
+				Type:  PanelLine,
+				Unit:  Unit{Format: UnitNumeric},
+				Query: `rate(jvm_threads_current{kubernetes_namespace='{{namespace}}',_weave_service='{{workload}}'}}[{{range}}])`,
+			}, {
+				Title: "Threads Created per Second",
+				Type:  PanelLine,
+				Unit:  Unit{Format: UnitNumeric},
+				Query: `rate(jvm_threads_started_total{kubernetes_namespace='{{namespace}}',_weave_service='{{workload}}'}}[{{range}}])`,
+			}},
+		}},
+	}, {
+		Name: "Memory",
+		Rows: []Row{{
+			Panels: []Panel{{
+				Title: "Used Memory",
+				Help:  "Used memory is working set (~live objects in the heap) + garbage",
+				Type:  PanelLine,
+				Unit:  Unit{Format: UnitBytes},
+				Query: `jvm_memory_bytes_used{kubernetes_namespace='{{namespace}}',_weave_service='{{workload}}'}`,
+			}, {
+				Title: "Memory Used per Pool",
+				Type:  PanelLine,
+				Unit:  Unit{Format: UnitBytes},
+				Query: `jvm_memory_pool_bytes_used{kubernetes_namespace='{{namespace}}',_weave_service='{{workload}}'}`,
+			}},
+		}},
+	}, {
+		Name: "Garbage Collector",
+		Rows: []Row{{
+			Panels: []Panel{{
+				Title: "Time Spent in GC every second",
+				Type:  PanelLine,
+				Unit:  Unit{Format: UnitSeconds},
+				Query: `rate(jvm_gc_collection_seconds_sum{kubernetes_namespace='{{namespace}}',_weave_service='{{workload}}'}[{{range}}])`,
+			}, {
+				Title: "Number of GC Cycles per Second",
+				Type:  PanelLine,
+				Unit:  Unit{Format: UnitNumeric},
+				Query: `rate(jvm_gc_collection_seconds_count{kubernetes_namespace='{{namespace}}',_weave_service='{{workload}}'}[{{range}}])`,
+			}},
+		}},
+	}},
+}
+
+var jvm = &staticProvider{
+	requiredMetrics: []string{
+		"jvm_threads_current",
+		"jvm_threads_started_total",
+		"jvm_memory_bytes_used",
+		"jvm_memory_pool_bytes_used",
+		"jvm_gc_collection_seconds_sum",
+		"jvm_gc_collection_seconds_count",
+	},
+	dashboard: jvmDashboard,
+}
