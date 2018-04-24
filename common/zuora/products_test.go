b8f11e008a2bda0b4fb37fc85578d556e7f1dcb2
Marc CARRE <carre.marc@gmail.com>
2018-04-24 15:43:09 +0100

    Factorise Zuora's mock client, and add support for multiple mock responses.

--- common/zuora/products_test.go
+++ common/zuora/products_test.go
@@ -1,10 +1,8 @@
 package zuora_test
 
 import (
-	"bytes"
 	"context"
 	"io/ioutil"
-	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,27 +12,11 @@ import (
 
 var conf = zuora.Config{SubscriptionPlanID: "plan-id"}
 
-type clientMock struct {
-	body       string
-	statusCode int
-}
-
-func (c *clientMock) Do(req *http.Request) (*http.Response, error) {
-	r := &http.Response{StatusCode: http.StatusOK}
-	if c.body != "" {
-		r.Body = ioutil.NopCloser(bytes.NewReader([]byte(c.body)))
-	}
-	if c.statusCode != 0 {
-		r.StatusCode = c.statusCode
-	}
-	return r, nil
-}
-
 func TestGetCurrentRates(t *testing.T) {
 	f, err := ioutil.ReadFile("testdata/zuora-catalog_products.json")
 	assert.NoError(t, err)
 
-	c := &clientMock{body: string(f)}
+	c := &mockClient{mockResponses: []mockResponse{{body: string(f)}}}
 	z := zuora.New(conf, c)
 
 	rates, err := z.GetCurrentRates(context.Background())
