6003fd448507d61939ce3ae7195dcdd17c9c55b2
Damien Lespiau <damien@weave.works>
2018-06-01 18:38:56 +0100

    dashboards: Add a dashboard for AWS RDS instances
    Fixes: #2051

---
+++ dashboard-api/dashboard/aws.go
@@ -0,0 +1,51 @@
+package dashboard
+
+var awsRDSDashboard = Dashboard{
+	ID:   "aws-rds",
+	Name: "RDS",
+	Sections: []Section{{
+		Name: "System",
+		Rows: []Row{{
+			Panels: []Panel{{
+				Title: "CPU utilization",
+				Type:  PanelLine,
+				Unit:  Unit{Format: UnitPercent},
+				Query: `sum(aws_rds_cpuutilization_average{kubernetes_namespace='{{namespace}}',_weave_service='{{workload}}',dbinstance_identifier='{{identifier}}'}) by (dbinstance_identifier)`,
+			}, {
+				Title: "Available RAM",
+				Type:  PanelLine,
+				Unit:  Unit{Format: UnitBytes},
+				Query: `sum(aws_rds_freeable_memory_average{kubernetes_namespace='{{namespace}}',_weave_service='{{workload}}',dbinstance_identifier='{{identifier}}'}) by (dbinstance_identifier)`,
+			}},
+		}},
+	}, {
+		Name: "Database",
+		Rows: []Row{{
+			Panels: []Panel{{
+				Title: "Number of connections in use",
+				Type:  PanelLine,
+				Unit:  Unit{Format: UnitNumeric},
+				Query: `sum(aws_rds_database_connections_average{kubernetes_namespace='{{namespace}}',_weave_service='{{workload}}',dbinstance_identifier='{{identifier}}'}) by (dbinstance_identifier)`,
+			}},
+		}},
+	}, {
+		Name: "Disk",
+		Rows: []Row{{
+			Panels: []Panel{{
+				Title: "Read IOPS",
+				Type:  PanelLine,
+				Unit:  Unit{Format: UnitNumeric},
+				Query: `sum(aws_rds_read_iops_average{kubernetes_namespace='{{namespace}}',_weave_service='{{workload}}',dbinstance_identifier='{{identifier}}'}) by (dbinstance_identifier)`,
+			}, {
+				Title: "Write IOPS",
+				Type:  PanelLine,
+				Unit:  Unit{Format: UnitNumeric},
+				Query: `sum(aws_rds_write_iops_average{kubernetes_namespace='{{namespace}}',_weave_service='{{workload}}',dbinstance_identifier='{{identifier}}'}) by (dbinstance_identifier)`,
+			}},
+		}},
+	}},
+}
+
+var awsRDS = &promqlProvider{
+	dashboard: &awsRDSDashboard,
+}
