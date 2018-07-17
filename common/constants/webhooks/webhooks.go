aac3e8395ef75e35880135fa7c22eced2af5f1ff
Aaron Kirkbride <aaron@aaronkirkbride.com>
2018-07-17 16:59:22 +0100

    Create tests for DockerHub and Quay webhook endpoints

--- common/constants/webhooks/webhooks.go
+++ common/constants/webhooks/webhooks.go
@@ -6,7 +6,7 @@ const WebhooksIntegrationTypeHeader = "X-Webhooks-Integration-Type"
 // GithubPushIntegrationType for github pushes which will ask flux to sync
 const GithubPushIntegrationType = "github.push"
 
-// GithubPushIntegrationType for dockerhub pushes which will ask flux to sync
+// DockerHubIntegrationType for dockerhub pushes which will ask flux to sync
 const DockerHubIntegrationType = "dockerhub.push"
 
 // QuayIntegrationType for quay pushes which will ask flux to sync
