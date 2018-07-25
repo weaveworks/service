9f0696ab29d99061ac202006e4e9077e9b555163
Marcus Cobden <marcus@weave.works>
2018-07-25 17:57:17 +0100

    Make URI and password flags more like other services
    Also switch to click from argparse

--- billing-exporter/exporter/__main__.py
+++ billing-exporter/exporter/__main__.py
@@ -1,37 +1,47 @@
 import sys
-import argparse
 import json
 import signal
 import logging
 from threading import Event, Thread
+from urllib.parse import urlparse
 
 from prometheus_client import start_http_server
 from google.oauth2 import service_account
+import click
 
 from .checks.access import AccessCheck
 from .checks.usage import UsageCheck
 
 
-def parse_args(args):
-    parser = argparse.ArgumentParser('billing-exporter')
-    parser.add_argument(
-        '--bigquery-creds-file', required=True, type=str)
-    parser.add_argument(
-        '--users-db-creds-file', required=True, type=argparse.FileType('r'))
-    parser.add_argument(
-        '--billing-db-creds-file', required=True, type=argparse.FileType('r'))
-    parser.add_argument(
-        '--zuora-creds-file', required=True, type=argparse.FileType('r'))
-    parser.add_argument(
-        '--production', type=bool, default=False)
-    parser.add_argument(
-        '--check-instance-id', type=str, nargs='+')
-    return parser.parse_args(args)
+_LOG = logging.getLogger(__name__)
 
 
-def load_json_file(fh):
-    with fh:
-        return json.load(fh)
+class UriParamType(click.ParamType):
+    name = 'uri'
+
+    def convert(self, value, param, ctx):
+        try:
+            return urlparse(value)
+        except ValueError as e:
+            self.fail(f'{value!r} is not a valid URL: {e}', param, ctx)
+
+
+def inject_password_from_file(name, uri, password_file):
+    if not password_file:
+        return uri
+
+    if uri.password:
+        _LOG.warn('Password for %s specified in both URI and password file', name)
+    with password_file as fh:
+        password = fh.read()
+
+    netloc = ''
+    if uri.username:
+        netloc += uri.username
+    netloc += f':{password}@{uri.hostname}'
+    if uri.port:
+        netloc += f':{uri.port}'
+    return uri._replace(netloc=netloc)
 
 
 def run_checker(checker, *args, **kwargs):
@@ -39,20 +49,29 @@ def run_checker(checker, *args, **kwargs):
         try:
             checker.run(*args, **kwargs)
         except Exception as e:
-            print(e)
+            _LOG.exception("Unhandled exception in checker %s", checker)
             raise
 
     t = Thread(target=_target)
     t.start()
 
 
-def main(args):
+@click.command()
+@click.option('--billing-database-uri', type=UriParamType())
+@click.option('--billing-database-password-file', type=click.File(), default=None)
+@click.option('--users-database-uri', type=UriParamType())
+@click.option('--users-database-password-file', type=click.File(), default=None)
+@click.option('--zuora-uri', type=UriParamType())
+@click.option('--zuora-password-file', type=click.File(), default=None)
+@click.option('--bigquery-creds-file', type=click.Path())
+@click.option('--production', is_flag=True, flag_value=True, default=False)
+@click.option('--check-instance-id', 'check_instance_ids', multiple=True)
+def main(
+        billing_database_uri, billing_database_password_file, 
+        users_database_uri, users_database_password_file,
+        zuora_uri, zuora_password_file, bigquery_creds_file,
+        production, check_instance_ids):
     logging.basicConfig()
-
-    opts = parse_args(args)
-
-    bq_creds = service_account.Credentials.from_service_account_file(
-        opts.bigquery_creds_file)
 
     should_stop = Event()
     def signal_handler():
@@ -60,18 +79,28 @@ def main(args):
 
     signal.signal(signal.SIGINT, signal_handler)
 
+    billing_db_uri = inject_password_from_file(
+        'billing', billing_database_uri, billing_database_password_file).geturl()
+    users_db_uri = inject_password_from_file(
+        'users', users_database_uri, users_database_password_file).geturl()
+    zuora_uri = inject_password_from_file(
+        'zuora', zuora_uri, zuora_password_file).geturl()
+
+    bq_creds = service_account.Credentials.from_service_account_file(
+        bigquery_creds_file)
+
     run_checker(
-        AccessCheck(opts.production),
+        AccessCheck(production),
         should_stop,
         bq_creds)
     run_checker(
-        UsageCheck(opts.production),
+        UsageCheck(production),
         should_stop,
-        opts.check_instance_id,
+        check_instance_ids,
         bq_creds,
-        load_json_file(opts.users_db_creds_file),
-        load_json_file(opts.billing_db_creds_file),
-        load_json_file(opts.zuora_creds_file))
+        users_db_uri,
+        billing_db_uri,
+        zuora_uri)
 
     # Start up the server to expose the metrics.
     start_http_server(8000)
