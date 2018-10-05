f86e475cf961b9485ffcd712027b3d8ac5e9b4f7
Marcus Cobden <marcus@weave.works>
2018-10-05 15:23:55 +0100

    Populate given/family name fields from existing name field
    It's not perfect, but it's better than nothing

--- users/db/migrations/038_add_column_users_given_family_names.up.sql
+++ users/db/migrations/038_add_column_users_given_family_names.up.sql
@@ -1,2 +1,25 @@
 ALTER TABLE users ADD COLUMN given_name text NOT NULL DEFAULT '';
 ALTER TABLE users ADD COLUMN family_name text NOT NULL DEFAULT '';
+
+-- This will split the full name into first/given and last/family names, and populate the fields accordingly
+-- It's not fool-proof or pretty, but it'll be good enough
+-- A lot of users have only entered single word names, so we take that name to be a first name
+UPDATE users
+SET given_name = trim(both ' ' from split_names.given_name),
+    family_name = trim(both ' ' from split_names.family_name)
+FROM (
+    WITH w_family_name AS (
+        select
+            id,
+            name,
+            reverse(substr(reverse(name),1,strpos(reverse(name),' '))) as family_name
+        FROM users
+        WHERE name != ''
+    )
+    SELECT
+        id,
+        substr(name, 1, length(name) - length(family_name)) as given_name,
+        family_name
+    from w_family_name
+) AS split_names
+WHERE users.id = split_names.id;
