875244952da804bdefc0a715635bd7fcec347422
Filip Barl <filip.barl@gmail.com>
2018-11-28 18:29:31 +0100

    Addressed Roli's comments.

--- users/db/migrations/043_modify_permission_tables.up.sql
+++ users/db/migrations/043_modify_permission_tables.up.sql
@@ -7,8 +7,5 @@ DROP TABLE user_team_role_memberships;
 DROP SEQUENCE user_team_role_memberships_id_seq;
 
 -- ... in favor of an extra column in team_memberships table (see https://github.com/weaveworks/service/pull/2409#discussion_r236929914)
-ALTER TABLE team_memberships ADD COLUMN role_id text REFERENCES roles(id);
-
--- Also create a new unique index for team_memberships that includes the role (to enforce single one) in favor of old unique index
-CREATE UNIQUE INDEX team_memberships_user_id_team_id_role_id_idx ON team_memberships (user_id, team_id, role_id) WHERE deleted_at IS NULL;
-DROP INDEX team_memberships_user_id_team_id_idx;
+-- TODO(fbarl): The default 'admin' should be removed once we enable permissions in the UI (see https://github.com/weaveworks/service/pull/2413/files#r237180045)
+ALTER TABLE team_memberships ADD COLUMN role_id text NOT NULL REFERENCES roles(id) DEFAULT 'admin';
