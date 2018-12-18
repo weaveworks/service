98fcd091a869e2751582c29553c2626a7834b055
Roland Schilter <roli@weave.works>
2018-12-18 15:40:21 -0800

    Skip team_memberships already existing

--- users/db/migrations/047_move_orphan_organizations_into_teams.up.sql
+++ users/db/migrations/047_move_orphan_organizations_into_teams.up.sql
@@ -7,9 +7,19 @@ WHERE organizations.team_id IS NULL AND CONCAT(organizations.external_id, '-team
 
 -- Move all user memberships into the new teams
 -- (implicitly linking the users to same organizations)
+--
+-- Note that some entries in `memberships` might already have an
+-- entry in `team_memberships` since organizations are allowed to
+-- be transferred into other teams. In that case, the entry in
+-- `memberships` was not removed.
 INSERT INTO team_memberships(team_id, user_id, created_at, deleted_at)
 SELECT organizations.team_id, memberships.user_id, memberships.created_at, memberships.deleted_at
-FROM memberships INNER JOIN organizations ON organizations.id = memberships.organization_id;
+FROM memberships
+  INNER JOIN organizations ON organizations.id = memberships.organization_id
+  INNER JOIN teams ON organizations.team_id = teams.id
+WHERE
+  teams.external_id LIKE '%-team'
+;
 
 -- Finally delete all the legacy direct user-organization memberships
 -- (we keep the table since it's still being referenced in the code)
