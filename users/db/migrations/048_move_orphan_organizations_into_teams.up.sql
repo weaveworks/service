a2c8cf311daae8c844598869730221c6db9c234c
Roland Schilter <roli@weave.works>
2018-12-20 22:17:51 -0800

    Handle duplicate organizations.external_id
    The constraint is on `deleted_at is not null` and as such can have
    duplicates. Since we determined assignment by just `{orgextid}-team` we
    ran into duplicate team external_ids.
    
    This PR additionally includes the `organizations.id` to make it
    unambiguous.

--- users/db/migrations/048_move_orphan_organizations_into_teams.up.sql
+++ users/db/migrations/048_move_orphan_organizations_into_teams.up.sql
@@ -3,7 +3,7 @@
 
 -- Move all orphan organizations into their dedicated teams
 UPDATE organizations SET team_id = teams.id FROM teams
-WHERE organizations.team_id IS NULL AND CONCAT(organizations.external_id, '-team') = teams.external_id;
+WHERE organizations.team_id IS NULL AND CONCAT(organizations.external_id, '-', organizations.id) = teams.external_id;
 
 -- Move all user memberships into the new teams
 -- (implicitly linking the users to same organizations)
@@ -18,7 +18,7 @@ FROM memberships
   INNER JOIN organizations ON organizations.id = memberships.organization_id
   INNER JOIN teams ON organizations.team_id = teams.id
 WHERE
-  teams.external_id LIKE '%-team'
+  teams.external_id LIKE '%-%-%-%' -- 'proud-wind-05-8168'
 ;
 
 -- Finally delete all the legacy direct user-organization memberships
