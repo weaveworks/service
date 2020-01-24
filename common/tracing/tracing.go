1c11951ad825e79663593e1f81fe61ef1c84be74
Bryan Boreham <bjboreham@gmail.com>
2020-01-24 14:14:27 +0000

    Ensure top-level tags are applied on traces sampled via feature flag
    Jaeger-client makes a sampling decision when the trace is created, and
    any tags or child spans are ignored when not-sampled.
    
    Fully resolving this is a chicken-and-egg problem in that we have to
    look up user details before we see the `trace-debug-id` feature-flag,
    but we can't do that until after the trace is created
    
    As a simple improvement, move some `SetTag` calls later, and re-apply
    the top-level method and url tags.

--- common/tracing/tracing.go
+++ common/tracing/tracing.go
@@ -2,21 +2,27 @@ package tracing
 
 import (
 	"context"
+	"net/http"
+
 	"github.com/weaveworks/service/common/featureflag"
 
 	opentracing "github.com/opentracing/opentracing-go"
-	opentracing_ext "github.com/opentracing/opentracing-go/ext"
+	ext "github.com/opentracing/opentracing-go/ext"
 )
 
 // ForceTraceIfFlagged Forces a trace collection if the "trace-debug-id" is among the provided feature flags
-func ForceTraceIfFlagged(ctx context.Context, featureFlags []string) {
+func ForceTraceIfFlagged(ctx context.Context, r *http.Request, featureFlags []string) {
 	debugID, found := featureflag.GetFeatureFlagValue("trace-debug-id", featureFlags)
 	if found {
 		if span := opentracing.SpanFromContext(ctx); span != nil {
+			ext.SamplingPriority.Set(span, 1) // need to do this before setting tags
 			if debugID != "" {
 				span.SetTag("trace-debug-id", debugID)
 			}
-			opentracing_ext.SamplingPriority.Set(span, 1)
+			// Now re-apply tags from nethttp
+			ext.HTTPMethod.Set(span, r.Method)
+			ext.HTTPUrl.Set(span, r.URL.String())
+			ext.Component.Set(span, "net/http")
 		}
 	}
 }
